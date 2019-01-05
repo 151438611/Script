0a903d838f583c5bc659dc33429448dee287ade2
Jun <33471088+151438611@users.noreply.github.com>
2019-01-05 13:51:55 +0800

    Update 6_rsync_for_N1_linux.sh

--- 6_rsync_for_N1_linux.sh
+++ 6_rsync_for_N1_linux.sh
@@ -10,25 +10,26 @@ rc_local=/etc/rc.local
 # script
 frpcini=/usr/local/apps/frp/frpc.ini
 frpcsh=/usr/local/apps/frp/frpc.sh
+rsyncsh=/usr/local/apps/rsync.sh
 # /opt
-nginx_conf=/opt/etc/nginx/nginx.conf
-phpini=/opt/etc/php.ini
-php_fpm_conf=/opt/etc/php-fpm.conf
-www_conf=/opt/etc/php7-fpm.d/www.conf
+#nginx_conf=/opt/etc/nginx/nginx.conf
+#phpini=/opt/etc/php.ini
+#php_fpm_conf=/opt/etc/php-fpm.conf
+#www_conf=/opt/etc/php7-fpm.d/www.conf
 
-source="$cron $rc_local $frpcini $frpcsh $nginx_conf $phpini $php_fpm_conf $www_conf"
+source="$cron $rc_local $frpcini $frpcsh $rsyncsh $nginx_conf $phpini $php_fpm_conf $www_conf"
 backup_dir=/media/sda1/Data/Config_bak ; [ -d "$backup_dir" ] || mkdir -p $backup_dir
 
 rsync_fun() {
-	# $1表示备份的源文件/目录 , $2表示备份的目的目录
-	  /usr/bin/rsync -trv $1 $2
-	    [ $? -eq 0 ] && echo "$(date +"%F %T") rsync success $1" >> $rsynclog || echo "$(date +"%F %T") rsync fail--- $1" >> $rsynclog
+# $1表示备份的源文件/目录 , $2表示备份的目的目录
+  /usr/bin/rsync -trv $1 $2
+  [ $? -eq 0 ] && echo "$(date +"%F %T") rsync success $1" >> $rsynclog || echo "$(date +"%F %T") rsync fail--- $1" >> $rsynclog
 }
-# =================== start rsync file ====================================
+# =================== start rsync file/dir ====================================
 for src in $source
 do
   rsync_fun $src $backup_dir
 done
 
 # ===temp use===
-[ $(date +%e) -eq 1 -o $(date +%e) -eq 15 ] && tar -zcf $backup_dir/opt_all.tgz /opt --exclude kodexplorer --exclude tmp
+[ -n "$(date +%e | grep -E "1|8|15|22")" ] && tar -zcf $backup_dir/opt_all.tgz /opt --exclude kodexplorer --exclude tmp
