7958537472751cff01399f6d342cccdd40b0af1e
Jun <33471088+151438611@users.noreply.github.com>
2019-01-05 14:16:54 +0800

    Update 6_rsync_for_N1_linux.sh

--- 6_rsync_for_N1_linux.sh
+++ 6_rsync_for_N1_linux.sh
@@ -6,18 +6,19 @@ grep -qi $(basename $0) $cron || echo "35 3 * * * sh /usr/local/apps/$(basename 
 rsynclog=/tmp/rsync.log ; echo "" >> $rsynclog
 
 # /etc
-rc_local=/etc/rc.local
+src0=$cron
+src1=/etc/rc.local
 # script
-frpcini=/usr/local/apps/frp/frpc.ini
-frpcsh=/usr/local/apps/frp/frpc.sh
-rsyncsh=/usr/local/apps/rsync.sh
+src2=/usr/local/apps/frp/frpc.ini
+src3=/usr/local/apps/frp/frpc.sh
+src4=/usr/local/apps/rsync.sh
 # /opt
-#nginx_conf=/opt/etc/nginx/nginx.conf
-#phpini=/opt/etc/php.ini
-#php_fpm_conf=/opt/etc/php-fpm.conf
-#www_conf=/opt/etc/php7-fpm.d/www.conf
+#src5=/opt/etc/nginx/nginx.conf
+#src6=/opt/etc/php.ini
+#src7=/opt/etc/php-fpm.conf
+#src8=/opt/etc/php7-fpm.d/www.conf
 
-source="$cron $rc_local $frpcini $frpcsh $rsyncsh $nginx_conf $phpini $php_fpm_conf $www_conf"
+source="$src0 $src1 $src2 $src3 $src4 $src5 $src6 $src7 $src8 $src9"
 backup_dir=/media/sda1/Data/Config_bak ; [ -d "$backup_dir" ] || mkdir -p $backup_dir
 
 rsync_fun() {
@@ -25,7 +26,7 @@ rsync_fun() {
   /usr/bin/rsync -tr $1 $2
   [ $? -eq 0 ] && echo "$(date +"%F %T") rsync success $1" >> $rsynclog || echo "$(date +"%F %T") rsync fail--- $1" >> $rsynclog
 }
-# =================== start rsync file/dir ====================================
+# =================== start rsync file ====================================
 for src in $source
 do
   rsync_fun $src $backup_dir
