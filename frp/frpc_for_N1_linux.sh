a68e4c6e2210aaec15125436ba5218c6775f551b
Jun <33471088+151438611@users.noreply.github.com>
2019-01-05 14:24:05 +0800

    Update frpc_for_N1_linux.sh

--- frp/frpc_for_N1_linux.sh
+++ frp/frpc_for_N1_linux.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+cron=/etc/cron/crontabs/root
+grep -qi $(basename $0) $cron || echo -e "\n15 * * * * [ \$(date +%k) -eq 5 ] && killall frpc ; sleep 8 && sh /usr/local/apps/frp/$(basename $0)" >> $cron
+
 frpc="/usr/local/apps/frp/frpc" ; frpcini="/usr/local/apps/frp/frpc.ini"
 [ -x "$frpc" -a -f "$frpcini"  ] || exit 1
 
@@ -10,7 +13,7 @@ old_addr=$(awk '$0~"local_ip = 192.168" {print $3}' $frpcini | head -n1)
 
 if [ -z "$(pidof frpc)" ] ; then
   $frpc -c $frpcini &
-  echo "$(date +"%F %T") frpc was not runing ; start frpc ..." >> /tmp/frpc.log
+    echo "$(date +"%F %T") frpc was not runing ; start frpc ..." >> /tmp/frpc.log
 else
-  echo "$(date +"%F %T") frpc is runing, Don't do anything !" >> /tmp/frpc.log
+    echo "$(date +"%F %T") frpc is runing, Don't do anything !" >> /tmp/frpc.log
 fi
