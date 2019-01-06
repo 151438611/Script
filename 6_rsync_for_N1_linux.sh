a153cf1a6ee0f75ab390f5ffccc9c991f8114fed
Jun <33471088+151438611@users.noreply.github.com>
2019-01-06 12:14:22 +0800

    Update 6_rsync_for_N1_linux.sh

--- 6_rsync_for_N1_linux.sh
+++ 6_rsync_for_N1_linux.sh
@@ -2,7 +2,7 @@
 # 
 
 cron=/etc/cron/crontabs/root
-grep -qi $(basename $0) $cron || echo "35 3 * * * sh /usr/local/apps/$(basename $0)" >> $cron
+grep -qi $(basename $0) $cron || echo -e "\n35 3 * * * sh /usr/local/apps/$(basename $0)" >> $cron
 rsynclog=/tmp/rsync.log ; echo "" >> $rsynclog
 
 # /etc
@@ -19,7 +19,7 @@ src4=/usr/local/apps/rsync.sh
 #src8=/opt/etc/php7-fpm.d/www.conf
 
 source="$src0 $src1 $src2 $src3 $src4 $src5 $src6 $src7 $src8 $src9"
-backup_dir=/media/sda1/Data/Config_bak ; [ -d "$backup_dir" ] || mkdir -p $backup_dir
+dest=/media/sda1/Data/Config_bak ; [ -d "$dest" ] || mkdir -p $dest
 
 rsync_fun() {
 # $1表示备份的源文件/目录 , $2表示备份的目的目录
@@ -29,8 +29,14 @@ rsync_fun() {
 # =================== start rsync file ====================================
 for src in $source
 do
-  rsync_fun $src $backup_dir
+  rsync_fun $src $dest
 done
 
 # ===temp use===
 [ -n "$(date +%e | grep -E "1|8|15|22")" ] && tar -zcf $backup_dir/opt_all.tgz /opt --exclude kodexplorer --exclude tmp
+
+# mount -t cifs -o username=admin,password=administrator //192.168.20.200/Public /media/hwnas
+mount_user=admin ; mount_pw=administrator
+mount_src=//192.168.20.200/Public
+mount_dest=/media/hwnas ; [ -d $mount_dest ] || mkdir -p $mount_dest
+[ -z "$(mount | grep $mount_src)" ] && mount -t cifs -o username=$mount_user,password=$mount_pw $mount_src $mount_dest
