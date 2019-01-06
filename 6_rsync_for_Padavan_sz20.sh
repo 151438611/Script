a321692bf10b6aae338357e301f2035df5df089e
Jun <33471088+151438611@users.noreply.github.com>
2019-01-06 18:18:33 +0800

    Update 6_rsync_for_Padavan_sz20.sh

--- 6_rsync_for_Padavan_sz20.sh
+++ 6_rsync_for_Padavan_sz20.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 将 hwnas 挂载到sz20_k2p路由器的目录 /tmp/hwnas 下, 使用 rsync 将 U盘 中的文件备份到 hwnas 中保存
+# 将 hwnas 挂载到szk2p路由器的目录下, 使用 rsync 将 U盘 中的文件备份到 hwnas 中保存
 
 cron=/etc/storage/cron/crontabs/$(nvram get http_username) 
 grep -qi $(basename $0) $cron || echo "35 3 * * * sh /etc/storage/bin/$(basename $0)" >> $cron
