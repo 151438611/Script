574f6bab35206fc1abf737435fbfa9f34bff65dc
Jun <33471088+151438611@users.noreply.github.com>
2019-01-09 14:01:53 +0800

    Update 6_rsync_for_N1_armbian.sh

--- 6_rsync_for_N1_armbian.sh
+++ 6_rsync_for_N1_armbian.sh
@@ -6,7 +6,6 @@ cron=/var/spool/cron/crontabs/root
 grep -qi $(basename $0) $cron || echo -e "\n35 3 * * * sh /opt/$(basename $0)" >> $cron
 rsynclog=/tmp/rsync.log ; echo "" >> $rsynclog
 
-
 src0=/etc/rc.local
 src1=/etc/nginx
 src2=/etc/php
@@ -23,8 +22,8 @@ dest=/media/sda1/Data/Config_bak ; [ -d "$dest" ] || mkdir -p $dest
 
 rsync_fun() {
 # $1表示备份的源文件/目录src , $2表示备份的目的目录dest
-/usr/bin/rsync -tr $1 $2
-[ $? -eq 0 ] && echo "$(date +"%F %T") rsync success $1" >> $rsynclog || echo "$(date +"%F %T") rsync fail--- $1" >> $rsynclog
+  rsync -tr $1 $2
+  [ $? -eq 0 ] && echo "$(date +"%F %T") rsync success $1" >> $rsynclog || echo "$(date +"%F %T") rsync fail--- $1" >> $rsynclog
 }
 
 # === start rsync file/dir ==============
@@ -32,7 +31,7 @@ for src in $source
  do
    rsync_fun $src $dest
  done
-/usr/bin/rsync -tr $cron $dest/crontab.txt
+rsync -tr $cron $dest/crontab.txt
 
 if [ -n "$(date +%e | grep -E "1|8|15|22")" ] ; then
   cd $dest && tar -zcvf ../Script/N1_armbian_Conf_backup.tgz * --exclude *.tgz
