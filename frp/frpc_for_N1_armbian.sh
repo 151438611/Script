867f32b0724e29ae1ca1420957a6ec496125d5be
Jun <33471088+151438611@users.noreply.github.com>
2019-01-09 13:33:43 +0800

    Update frpc_for_N1_armbian.sh

--- frp/frpc_for_N1_armbian.sh
+++ frp/frpc_for_N1_armbian.sh
@@ -8,14 +8,13 @@ grep -qi reboot $cron || echo -e "\n5 5 * * * [ -n \"\$(date +\\%e | grep -E \"1
 frpc="/opt/frp/frpc" ; frpcini="/opt/frp/frpc.ini"
 [ -x "$frpc" -a -f "$frpcini"  ] || exit 1
 
-ip_addr=$(ifconfig eth0 | awk '$0~"inet addr"{print $2}' | cut -d: -f2)
+ip_addr=$(ifconfig eth0 | awk '/inet/ && /netmask/ && /broadcast/{print $2}')
 old_addr=$(awk '$0~"local_ip = 192.168" {print $3}' $frpcini | head -n1)
-
 [ -n "$ip_addr" -a "$ip_addr" != "$old_addr" ] && sed -i 's/'"$old_addr"'/'"$ip_addr"'/g' $frpcini
 
 if [ -z "$(pidof frpc)" ] ; then
   $frpc -c $frpcini &
-    echo "$(date +"%F %T") frpc was not runing ; start frpc ..." >> /tmp/frpc.log
+  echo "$(date +"%F %T") frpc was not runing ; start frpc ..." >> /tmp/frpc.log
 else
-    echo "$(date +"%F %T") frpc is runing, Don't do anything !" >> /tmp/frpc.log
+  echo "$(date +"%F %T") frpc is runing, Don't do anything !" >> /tmp/frpc.log
 fi
