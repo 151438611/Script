27c1950716ccfa2cc35de643579210962770314e
Jun <33471088+151438611@users.noreply.github.com>
2019-01-20 14:03:27 +0800

    Update frpc_for_N1_armbian.sh

--- frp/frpc_for_N1_armbian.sh
+++ frp/frpc_for_N1_armbian.sh
@@ -1,12 +1,50 @@
 #!/bin/sh
-PATH=$PATH:/usr/local/bin:/usr/local/sbin
+#export PATH=$PATH:/usr/local/bin:/usr/local/sbin
 
 cron=/var/spool/cron/crontabs/root
 grep -qi $(basename $0) $cron || echo -e "\n15 * * * * [ \$(date +\\%k) -eq 5 ] && /usr/bin/killall frpc ; sleep 8 && sh /opt/frp/$(basename $0)" >> $cron
-grep -qi reboot $cron || echo -e "\n5 5 * * * [ -n \"\$(date +\\%e | grep -E \"10|20|28\")\" ] && /sbin/reboot || ping -c2 -w5 114.114.114.114 || /sbin/reboot" >> $cron
+grep -qi reboot $cron || echo -e "\n5 5 * * * [ \$(date +\\%u) -eq 6 ] && /sbin/reboot || ping -c2 -w5 114.114.114.114 || /sbin/reboot" >> $cron
 
-frpc="/opt/frp/frpc" ; frpcini="/opt/frp/frpc.ini"
-[ -x "$frpc" -a -f "$frpcini"  ] || exit 1
+# -----1、填写服务端的IP/域名、认证密码即可---------------------------
+server_addr=frp.xiongxinyi.cn ; token=administrator
+name=Phicomm_N1 ; subdomain=kodexplorer
+frpc=/opt/frp/frpc ; frpcini=/opt/frp/frpc.ini
+frpc_url=http://14.116.146.30:11111/file/frp/frpc_linux_arm64 && md5_frpc=6c0ed666fe51b6216d22531824567986
+
+download_frpc() {
+  rm -f $frpc ; wget -O $frpc $frpc_url &
+  sleep 60 ; killall -q wget curl
+}
+[ "$(md5sum $frpc | cut -d " " -f 1)" = "$md5_frpc"] || download_frpc ; chmod 555 $frpc
+
+if [ ! -f "$frpcini" ] ; then
+cat << END > $frpcini
+[common]
+server_addr = $server_addr
+server_port = 7000
+protocol = tcp
+token = $token
+user = $name
+pool_count = 8
+tcp_mux = true
+login_fail_exit = true
+# ----- SSH_port:22 / Telnet_port:23 / RemoteDesktop_port:3389 -----
+[ssh]
+type = tcp
+local_ip = 127.0.0.1
+local_port = 22
+remote_port = 0
+use_encryption = false
+use_compression = true
+# ------------ http Tunnel config ------------
+[$subdomain]
+type = tcp
+local_ip = 127.0.0.1
+local_port = 80
+remote_port = 0
+use_encryption = false
+use_compression = true
+END
 
 ip_addr=$(ifconfig eth0 | awk '/inet/ && /netmask/ && /broadcast/{print $2}')
 old_addr=$(awk '$0~"local_ip = 192.168" {print $3}' $frpcini | head -n1)
