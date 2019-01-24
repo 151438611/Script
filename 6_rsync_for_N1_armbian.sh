ac1fc14d63a6c8e1525a158920c1a0a5199cb718
Jun <33471088+151438611@users.noreply.github.com>
2019-01-24 22:50:54 +0800

    Update 6_rsync_for_N1_armbian.sh

--- 6_rsync_for_N1_armbian.sh
+++ 6_rsync_for_N1_armbian.sh
@@ -20,7 +20,7 @@ src10=
 src11=
 
 source="$src0 $src1 $src2 $src3 $src4 $src5 $src6 $src7 $src8 $src9 $src10 $src11"
-dest=/media/sda1/Data/Config_bak ; [ -d "$dest" ] || mkdir -p $dest
+dest=/media/sda1/data/config_bak ; [ -d "$dest" ] || mkdir -p $dest
 
 rsync_fun() {
 # $1表示备份的源文件/目录src , $2表示备份的目的目录dest
@@ -36,7 +36,7 @@ for src in $source
    rsync_fun $cron $dest/crontab.txt
 
 if [ -n "$(date +%u | grep 5)" ] ; then
-  cd $dest && tar -zcvf ../Script/N1_armbian_Conf_backup.tgz *
+  cd $dest && tar -zcvf ../script/conf_backup_armbian_n1.tgz *
 fi
 
 chown -R armbian.armbian /media/sda1
@@ -45,7 +45,7 @@ chown -R armbian.armbian /media/sda1
 sh /opt/mount_smb.sh
 if [ -n "$(mount | grep 10gtek)" ] ; then
   cd /media/sda1/
-  tar -zcvf /media/10gtek/backup$(date +%Y%m%d).tgz --exclude Photo Data
+  tar -zcvf /media/10gtek/backup$(date +%Y%m%d).tgz --exclude photo data
   [ `echo $?` -eq 0 ] && echo "$(date +"%F %T") backup to 10gtek success ! " >> $rsynclog
   sleep 60 && find /media/10gtek -type f -name "backup*" -ctime +5 -exec rm -f {} \; 
 else
