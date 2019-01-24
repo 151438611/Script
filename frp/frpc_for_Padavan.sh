c3f6733866f4d5b6eea1639ba0fa6e63618a1368
Jun <33471088+151438611@users.noreply.github.com>
2019-01-24 20:16:48 +0800

    Update frpc_for_Padavan.sh

--- frp/frpc_for_Padavan.sh
+++ frp/frpc_for_Padavan.sh
@@ -25,7 +25,7 @@ http_file_enable=0 ; if [ $http_file_enable -eq 1 ] ; then http_file_path=$udisk
 ttyd_url="http://xiongxinyi.cn:2015/tools/frp/ttyd_mipsle"  && md5_ttyd=d1484e8e97adf6c2ca9cc1067c9cded6
 frpc_url1="http://14.116.146.30:11111/file/frp/frpc_mipsle" && md5_frpc1=c1513b5e3b8b7ff902c1ec067924ac02
 frpc_url2="http://14.116.146.30:12222/file/frp/frpc_mipsle" && md5_frpc2=c1513b5e3b8b7ff902c1ec067924ac02
-#frpc_url3="http://opt.cn2qq.com/opt-file/frpc"              && md5_frpc3=964c16fbe3edaa12674cee9b1c41f0f0
+frpc_url3="http://opt.cn2qq.com/opt-file/frpc"              && md5_frpc3=964c16fbe3edaa12674cee9b1c41f0f0
 md5_frpc="$md5_frpc1 $md5_frpc2 $md5_frpc3 55e7fe21e4924e323f539266cbfdfe38"
 frpc="$udisk/frpc" ; frpcini=/tmp/frpc.ini
 frpcini="$bin_dir/frpc.ini" 
@@ -44,7 +44,7 @@ fi
 # -------------------------- frpc ---------------------------------------------------
 download_frpc() {
   rm -f $frpc ; wget -O $frpc $frpc_url1 &
-  sleep 60 ; killall -q frpc wget curl
+  sleep 60 ; killall -q frpc wget
   if [ "$(md5sum $frpc | cut -d " " -f 1)" != "$md5_frpc1" ] ; then
     rm -f $frpc ; wget -O $frpc $frpc_url2 &
     sleep 60 ; killall -q wget curl
