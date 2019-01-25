1d96e37dda3310f7a1ac432c148a11cfc3dd5a43
Jun <33471088+151438611@users.noreply.github.com>
2019-01-25 23:45:54 +0800

    Update and rename 9_scan_host_ping.sh to 9_scan_host.sh

--- 9_scan_host.sh
+++ 9_scan_host.sh
@@ -5,18 +5,17 @@ subnet=192.168.3.
 hosts=/tmp/online_host.txt ; echo "$(arp -n | head -n1)" > $hosts
 
 a=$1 ; b=$2
-[ -z "$a" ] && echo "please input Number" && exit
-expr $a + 1 &> /dev/null && expr $b + 1 &> /dev/null
-[ $? -ne 0 ] && echo "please input Number " && exit 
-if [ "$a" = 0 ] ; then
+
+[ -n "$(echo $a | tr -d [0-9])" -o -n "$(echo $b | tr -d [0-9])" -o -z "$a" ] && echo "please input Number" && exit
+
+if [ "$a" = 0 ] ; then unset a
   [ -z "$b" -o "$b" = 0 ] && echo "please input not zero Number" && exit
-  unset a
 elif [ "$b" = 0 ] ; then unset b
 elif [ -n "$b" ] ; then 
 	[ "$a" -gt "$b" ] && c=$a && a=$b && b=$c
-  [ "$b" -ge 255 ] && echo "It's cann't grate than 255" && exit
+    [ "$b" -ge 255 ] && echo "It's cann't grate than 255" && exit
 fi
-[ -n "$a" ] && [ "$a" -ge 255 ] && echo "It's cann't grate than 255" && exit
+[ "$a" -ge 255 ] && echo "It's cann't grate than 255" && exit
 
 for x in `seq $a $b`
 do
