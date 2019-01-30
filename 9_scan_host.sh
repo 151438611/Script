a15d673748cfb2c9d6a32f3a6b22b69be1ca9eea
Jun <33471088+151438611@users.noreply.github.com>
2019-01-30 10:46:29 +0800

    Update 9_scan_host.sh

--- 9_scan_host.sh
+++ 9_scan_host.sh
@@ -2,20 +2,20 @@
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin
 
 subnet="192.168.3."
-hosts=/tmp/online_host.txt ; echo echo $(date +"%F %T") > $hosts
+hosts=/tmp/online_host.txt ; echo "$(date +"%F %T") start scan ...... " > $hosts
 
 a=$1 ; b=$2
 
-[ -n "$(echo $a | tr -d [0-9])" -o -n "$(echo $b | tr -d [0-9])" -o -z "$a" ] && echo "please input Number" && exit
+[ -n "$(echo $a | tr -d [0-9])" -o -n "$(echo $b | tr -d [0-9])" -o -z "$a" ] && echo "please input subnet_number" && exit
 
 if [ "$a" = 0 ] ; then unset a
-  [ -z "$b" -o "$b" = 0 ] && echo "please input not zero Number" && exit
+  [ -z "$b" -o "$b" = 0 ] && echo "please input not zero subnet_number" && exit
 elif [ "$b" = 0 ] ; then unset b
 elif [ -n "$b" ] ; then 
   [ "$a" -gt "$b" ] && c=$a && a=$b && b=$c
-  [ "$b" -ge 255 ] && echo "It's MaxNum cann't grate than 255" && exit
+  [ "$b" -ge 255 ] && echo "It's Max subnet_number cann't grate than 255" && exit
 fi
-[ "$a" -ge 255 ] && echo "It's MinNum cann't grate than 255" && exit
+[ "$a" -ge 255 ] && echo "It's Min subnet_number cann't grate than 255" && exit
 
 for x in `seq $a $b`
 do
