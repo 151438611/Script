69730bf72f22793c3d188bf5d3cba7419a71df6a
Jun <33471088+151438611@users.noreply.github.com>
2019-02-18 08:42:15 +0800

    Update 1_frpc_for_OpenWrt.sh

--- 1_frpc_for_OpenWrt.sh
+++ 1_frpc_for_OpenWrt.sh
@@ -7,15 +7,13 @@ if [ $(grep -c $(basename $0) $startup) -eq 0 ] ; then
   echo "sleep 40 ; wget -P /tmp/ $frpc_sh && mv -f /tmp/frpc.sh /etc/ ; sh /etc/frpc.sh" >> $startup
   echo "exit 0" >> $startup
 fi
-grep -qi "reboot" $cron || echo "5 5 * * * [ -n \"\$(date +%d | grep -E \"10|20|28\")\" ] && reboot || ping -c2 -w5 114.114.114.114 || reboot" >> $cron
-grep -qi "frpc.sh" $cron || echo "10 * * * * [ \$(date +%k) -eq 6 ] && killall -q frpc ; sh /etc/frpc.sh" >> $cron
+grep -qi reboot $cron || echo "5 5 * * * [ -n \"\$(date +%d | grep -E \"10|20|28\")\" ] && reboot || ping -c2 -w5 114.114.114.114 || reboot" >> $cron
+grep -qi $(basename $0) $cron || echo "10 * * * * [ \$(date +%k) -eq 6 ] && killall -q frpc ; sh /etc/frpc.sh" >> $cron
 name=$(uci get system.@system[0].hostname) ; lanip=$(uci get network.lan.ipaddr) && i=$(echo $lanip | cut -d . -f 3)
 
 # -----1、填写服务端的IP/域名、认证密码即可---------------------------
 server_addr="frp.xiongxinyi.cn" ; token="administrator"  ; subdomain="${name:0:2}$i" 
-# -----2、是否添加Telnet(远程桌面)、简单的http_file文件服务: 0表示不开启，1表示开启 ; 如果开启后面的参数都需要更改-----
-telnet_enable=0 ; if [ $telnet_enable -eq 1 ] ; then telnet_local_ip=192.168.11.10 ; telnet_local_port=23 ; telnet_remote_port=0 ; fi
-# -----3、frpc的下载地址、frpcini设置临时配置(默认/tmp/重启自动更新)还是永久保存配置(/etc/storage/，需取消注释#)-----
+# -----2、frpc的下载地址、frpcini设置临时配置(默认/tmp/重启自动更新)还是永久保存配置(/etc/storage/，需取消注释#)-----
 frpc_url1=http://14.116.146.30:11111/file/frp/frpc_linux_mips && md5_frpc1=756a5320f75b98b12f5bea1846f8d3fa
 frpc_url2=http://14.116.146.30:12222/file/frp/frpc_linux_mips && md5_frpc2=756a5320f75b98b12f5bea1846f8d3fa
 md5_frpc="$md5_frpc1 $md5_frpc2"
