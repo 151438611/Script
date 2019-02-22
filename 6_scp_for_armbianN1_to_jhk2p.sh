4ba4423788c9563e07698217f0d699e88c57a36f
Jun <33471088+151438611@users.noreply.github.com>
2019-02-22 22:26:57 +0800

    Update 6_scp_for_armbianN1_to_jhk2p.sh

--- 6_scp_for_armbianN1_to_jhk2p.sh
+++ 6_scp_for_armbianN1_to_jhk2p.sh
@@ -1,11 +1,36 @@
 #!/bin/sh
-#
+# 使用scp配合ssh公钥私钥，用来将armbian中的文件同步到jhk2p路由器中保存
 
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin
+scplog=/tmp/scp.log ; echo "" >> $rsynclog
 
-src=/opt/frp/frpc
-dest=/media/AiDisk_a2/tmp
+src0=/media/sda1/data/script
+src1=/media/sda1/data/document
+src2=/media/sda1/data/config_bak
+src3=
+src4=
+src5=
+src6=
+src7=
+src8=
+src9=
+src10=
+source="$src0 $src1 $src2 $src3 $src4 $src5 $src6 $src7 $src8 $src9 $src10"
+
 dest_ip=admin@frp.xiongxinyi.cn
+dest_dir=/media/AiDisk_a2/data
+dest=$dest_ip:$dest_dir
 dest_port=17500
-scp -C -P $dest_port $src $dest_ip:$dest
 
+scp_fun() {
+# $1表示备份的源文件/目录src , $2表示备份的目的目录dest
+  scp -P $dest_port $1 $2
+  [ $? -eq 0 ] && echo "$(date +"%F %T") scp success $1" >> $scplog || echo "$(date +"%F %T") scp fail--- $1" >> $scplog
+}
+
+# === start scp ==============
+for src in $source
+ do
+   scp_fun $src $dest
+ done
+ 
