80b5761a8972a1fec6db93b6e08612d6da355c4f
Jun <33471088+151438611@users.noreply.github.com>
2019-02-23 09:14:59 +0800

    Update 1_frpc_for_armbianN1.sh

--- 1_frpc_for_armbianN1.sh
+++ 1_frpc_for_armbianN1.sh
@@ -1,29 +1,39 @@
 #!/bin/sh
 #
-export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+# armbian中crontab $PATH=/usr/bin:/bin
+export PATH=/bin:/sbin:/usr/bin:/usr/sbin:$PATH
 
 cron=/var/spool/cron/crontabs/root
-grep -qi $(basename $0) $cron || echo -e "\n15 * * * * [ \$(date +\\%k) -eq 5 ] && /usr/bin/killall frpc ; sleep 8 && sh /opt/frp/$(basename $0)" >> $cron
 grep -qi reboot $cron || echo -e "\n5 5 * * * [ \$(date +\\%u) -eq 6 ] && /sbin/reboot" >> $cron
 
-# -----1、填写服务端的IP/域名、认证密码即可---------------------------
-server_addr=frp.xiongxinyi.cn ; token=administrator
-name=armbian_n1 ; subdomain=kodexplorer
+cron_frpc="15 * * * * [ \$(date +\\%k) -eq 5 ] && killall -q frpc ; sleep 8 && sh /opt/frp/$(basename $0)"
+grep -qi $(basename $0) $cron || echo -e "\n$cron_frpc" >> $cron
 
-frpc=/opt/frp/frpc ; frpcini=/opt/frp/frpc.ini
+# -----1、填写服务端的IP/域名、认证密码即可---------------------------
+server_addr=frp.xiongxinyi.cn
+token=administrator
+name=10gtek_n1
+subdomain=kodexplorer
+
+frpc=/opt/frp/frpc
+frpcini=/opt/frp/frpc.ini
 frpc_url=http://14.116.146.30:11111/file/frp/frpc_linux_arm64 && md5_frpc1=1610d1011fece9d806c8e3ba5dd2ad8f
 
-ttyd=/opt/frp/ttyd ; ttyd_url=http://14.116.146.30:11111/file/frp/ttyd_linux.aarch64
+ttyd=/opt/frp/ttyd
+ttyd_url=http://14.116.146.30:11111/file/frp/ttyd_linux.aarch64
 if [ -z $(pidof ttyd) ] ; then
-  [ -f "$ttyd" ] || wget -O $ttyd $ttyd_url ; chmod 555 $ttyd
+  [ -f "$ttyd" ] || wget -O $ttyd $ttyd_url
+  chmod 555 $ttyd
   $ttyd -p 7682 -m 5 -d 1 /bin/login &
 fi
 
 download_frpc() {
-  rm -f $frpc ; wget -O $frpc $frpc_url &
+  rm -f $frpc
+  wget -O $frpc $frpc_url &
   sleep 60 ; killall -q wget
 }
-[ "$(md5sum $frpc | cut -d " " -f 1)" = "$md5_frpc1" ] || download_frpc ; chmod 555 $frpc
+[ "$(md5sum $frpc | cut -d " " -f 1)" = "$md5_frpc1" ] || download_frpc
+chmod 755 $frpc
 
 if [ ! -f "$frpcini" ] ; then
 cat << END > $frpcini
@@ -54,10 +64,6 @@ use_encryption = false
 use_compression = true
 END
 fi
-
-#ip_addr=$(ifconfig eth0 | awk '/inet/ && /netmask/ && /broadcast/{print $2}')
-#old_addr=$(awk '$0~"local_ip = 192.168" {print $3}' $frpcini | head -n1)
-#[ -n "$ip_addr" -a -n "$old_addr" ] && [ "$ip_addr" != "$old_addr" ] && sed -i 's/'"$old_addr"'/'"$ip_addr"'/g' $frpcini
 
 if [ -z "$(pidof frpc)" ] ; then
   $frpc -c $frpcini &
