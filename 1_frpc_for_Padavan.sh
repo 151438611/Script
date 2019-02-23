ab2ca8106a4008d1773322558cf85989a2b85081
Jun <33471088+151438611@users.noreply.github.com>
2019-02-23 08:52:17 +0800

    Update 1_frpc_for_Padavan.sh

--- 1_frpc_for_Padavan.sh
+++ 1_frpc_for_Padavan.sh
@@ -9,11 +9,11 @@ cron="/etc/storage/cron/crontabs/$user" ; startup="/etc/storage/started_script.s
 cron_reboot="5 5 * * * [ -n \"\$(date +%d | grep 5)\" ] && reboot || ping -c2 -w5 114.114.114.114 || reboot"
 grep -qi "reboot" $cron || echo -e "\n$cron_reboot" >> $cron
 
+cron_frpc="20 * * * * [ \$(date +%k) -eq 5 ] && killall -q frpc ; sleep 8 && sh $bin_dir/$(basename $0)"
+grep -qi $(basename $0) $cron || echo -e "\n$cron_frpc" >> $cron
+
 startup_frpc="sleep 60 ; wget -P /tmp/ $frpc_sh && mv -f /tmp/$(basename $frpc_sh) $bin_dir/$(basename $0) ; sh $bin_dir/$(basename $0)"
 grep -qi $(basename $0) $startup || echo -e "\n$startup_frpc" >> $startup
-
-cron_frpc="6 * * * * [ \$(date +%k) -eq 6 ] && killall -q frpc ; sleep 8 && sh $bin_dir/$(basename $0)"
-grep -qi $(basename $0) $cron || echo -e "\n$cron_frpc" >> $cron
 
 # 开启从wan口访问路由器和ssh服务(默认关闭)，即从上级路由直接访问下级路由或ssh服务
 #[ $(nvram get misc_http_x) -eq 0 ] && nvram set misc_http_x=1 && nvram set misc_httpport_x=80 && nvram commit
