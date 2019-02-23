09c80bbfa87c64b3b421bc5b663cfb97fdfcfdff
Jun <33471088+151438611@users.noreply.github.com>
2019-02-23 09:50:15 +0800

    Update 1_frpc_for_Padavan.sh

--- 1_frpc_for_Padavan.sh
+++ 1_frpc_for_Padavan.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # for Padavan
 # ------------------------- add crontab、startup、enable SSH -----------------------
-bin_dir="/etc/storage/bin" ; [ -d "$bin_dir" ] || mkdir -p $bin_dir
+bin_dir=/etc/storage/bin ; [ -d "$bin_dir" ] || mkdir -p $bin_dir
 user_name=$(nvram get http_username)
-cron="/etc/storage/cron/crontabs/$user_name"
-startup="/etc/storage/started_script.sh" ; 
-frpc_sh="http://14.116.146.30:11111/file/frp/frpc_padavan.sh"
+cron=/etc/storage/cron/crontabs/$user_name
+startup=/etc/storage/started_script.sh
+frpc_sh=http://14.116.146.30:11111/file/frp/frpc_padavan.sh
 
 cron_reboot="5 5 * * * [ -n \"\$(date +%d | grep 5)\" ] && reboot || ping -c2 -w5 114.114.114.114 || reboot"
 grep -qi "reboot" $cron || echo -e "\n$cron_reboot" >> $cron
@@ -25,7 +25,7 @@ lanip=$(nvram get lan_ipaddr) && i=$(echo $lanip | cut -d . -f 3)
 udisk=$(mount | awk '$1~"/dev/" && $3~"/media/"{print $3}' | head -n1) ; udisk=${udisk:=/tmp}
 
 # -----1、填写服务端的IP/域名、认证密码即可---------------------------
-server_addr="frp.xiongxinyi.cn" ; token="administrator"  ; subdomain="${host_name:0:2}$i"
+server_addr=frp.xiongxinyi.cn ; token=administrator  ; subdomain=$host_name$i
 
 # -----2、是否开启ttyd(web_ssh)、Telnet(或远程桌面)、简单的http_file文件服务; 0表示不开启，1表示开启 ------------
 ttyd_enable=0
@@ -34,14 +34,14 @@ http_file_enable=0
 if [ $http_file_enable -eq 1 ] ; then http_file_path=$udisk ; http_file_port=$(date +1%M%S) ; fi
 
 # -----3、ttyd、frpc的下载地址、frpcini设置临时配置(默认/tmp/)还是永久保存配置(/etc/storage/)----------------
-ttyd_url="http://14.116.146.30:11111/file/frp/ttyd_linux_mipsle"  && md5_ttyd=d1484e8e97adf6c2ca9cc1067c9cded6
-frpc_url1="http://14.116.146.30:11111/file/frp/frpc_linux_mipsle" && md5_frpc1=2bced9b3084f251b3fd2ca42cc472f6c
-frpc_url2="http://14.116.146.30:12222/file/frp/frpc_linux_mipsle" && md5_frpc2=2bced9b3084f251b3fd2ca42cc472f6c
-frpc_url3="http://opt.cn2qq.com/opt-file/frpc" && md5_frpc3=964c16fbe3edaa12674cee9b1c41f0f0
+ttyd_url=http://14.116.146.30:11111/file/frp/ttyd_linux_mipsle  && md5_ttyd=d1484e8e97adf6c2ca9cc1067c9cded6
+frpc_url1=http://14.116.146.30:11111/file/frp/frpc_linux_mipsle && md5_frpc1=2bced9b3084f251b3fd2ca42cc472f6c
+frpc_url2=http://14.116.146.30:12222/file/frp/frpc_linux_mipsle && md5_frpc2=2bced9b3084f251b3fd2ca42cc472f6c
+frpc_url3=http://opt.cn2qq.com/opt-file/frpc" && md5_frpc3=964c16fbe3edaa12674cee9b1c41f0f0
 md5_frpc="$md5_frpc1 $md5_frpc2 $md5_frpc3 df4538b0ccd828457af33b2e599ea87a"
-frpc="$udisk/frpc" ; frpcini="$bin_dir/frpc.ini" 
+frpc=$udisk/frpc ; frpcini=$bin_dir/frpc.ini
 
-# -------------------------- ttyd ----------------------------------------
+# -------------------------- ttyd -----------------------------
 download_ttyd() {
   killall -q ttyd ; rm -f $ttyd
   wget -O $ttyd $ttyd_url
@@ -53,7 +53,7 @@ if [ $ttyd_enable -eq 1 ] ; then
       $ttyd -p $ttyd_port -r 10 -m 3 -d 1 /bin/login &
   fi
 fi
-# -------------------------- frpc -----------------------------------------
+# -------------------------- frpc -----------------------------
 download_frpc() {
   rm -f $frpc ; wget -O $frpc $frpc_url1 &
   sleep 60 ; killall -q frpc wget
@@ -69,7 +69,7 @@ download_frpc() {
 frpc_md5sum=$(md5sum $frpc | cut -d " " -f 1)
 [ -n "$(echo "$md5_frpc" | grep ${frpc_md5sum:-null})" ] || download_frpc
 chmod 755 $frpc
-# ------------------------- frpc.ini -----------------------------------
+# ------------------------- frpc.ini -------------------------
 if [ ! -f "$frpcini" ] ; then
 cat << END > $frpcini
 [common]
@@ -111,7 +111,7 @@ echo -e "plugin_local_path = $http_file_path \nplugin_strip_prefix = file " >> $
 echo -e "plugin_http_user = \nplugin_http_passwd = \n" >> $frpcini
   fi
 fi
-# ------------------------- start frpc ---------------------------------
+# ------------------------- start frpc ---------------------
 ping -c2 -w5 114.114.114.114 && \
 if [ -z "$(pidof frpc)" ] ; then
      logger -t frpc "frpc is not running ; starting frpc......"
