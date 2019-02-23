3636ff9f07bc26e1fdf89ed35bf29b16fa85287e
Jun <33471088+151438611@users.noreply.github.com>
2019-02-23 10:41:18 +0800

    Update 4_filebrowser_for_Padavan.sh

--- 4_filebrowser_for_Padavan.sh
+++ 4_filebrowser_for_Padavan.sh
@@ -3,14 +3,15 @@
 
 user=$(nvram get http_username) ; cron=/etc/storage/cron/crontabs/$user ; startup=/etc/storage/started_script.sh
 grep -qi $(basename $0) $startup || echo "sleep 5 ; sh /etc/storage/bin/$(basename $0)" >> $startup
-grep -qi $(basename $0) $cron || echo "40 * * * * [ \$(date +%k) -eq 5 ] && killall -q filebrowser ; sleep 8 && sh /etc/storage/bin/$(basename $0)" >> $cron
+cron_filebrowser="35 * * * * [ \$(date +%k) -eq 5 ] && killall -q filebrowser ; sleep 8 && sh /etc/storage/bin/$(basename $0)"
+grep -qi $(basename $0) $cron || echo "$cron_filebrowser" >> $cron
 
 # ----- filebrowser_mipsle的下载地址 ------------------------------------
-fb_url="http://opt.cn2qq.com/opt-file/filemanager" && md5_fb="957c409aba8623ff6a5b6ed4b8b6045d"
+fb_url=http://opt.cn2qq.com/opt-file/filemanager && md5_fb=957c409aba8623ff6a5b6ed4b8b6045d
 port=2019
 
 udisk=$(mount | awk '/dev/ && /media/ {print $3}' | head -n1)
-[ -z "$udisk" ] && echo "U_disk is not exist !" >> /tmp/filebrowser.log && exit
+[ -z "$udisk" ] && echo "U_disk is not exist , exit !" >> /tmp/filebrowser.log && exit
 
 filebrowser="$udisk/filebrowser" && dir_fb=$(dirname $filebrowser)
 [ -d "$dir_fb" ] || mkdir -p $dir_fb 
