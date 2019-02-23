bd036996dd1fc1ac8edacf1ba1be92959540bb63
Jun <33471088+151438611@users.noreply.github.com>
2019-02-23 10:35:30 +0800

    Update 6_scp_for_armbianN1_to_jhk2p.sh

--- 6_scp_for_armbianN1_to_jhk2p.sh
+++ 6_scp_for_armbianN1_to_jhk2p.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # 使用scp配合ssh公钥私钥，用来将armbian中的文件同步到jhk2p路由器中保存
 
-#export PATH=/bin:/sbin:/usr/bin:/usr/sbin
+export PATH=/bin:/sbin:/usr/bin:/usr/sbin:$PATH
 scplog=/tmp/scp.log ; echo "" >> $scplog
 
 src0=/media/sda1/data/script
@@ -17,10 +17,10 @@ src9=
 src10=
 source="$src0 $src1 $src2 $src3 $src4 $src5 $src6 $src7 $src8 $src9 $src10"
 
+dest_port=17500
 dest_ip=admin@frp.xiongxinyi.cn
 dest_dir=/media/AiDisk_a2/data
 dest=$dest_ip:$dest_dir
-dest_port=17500
 
 scp_fun() {
 # $1表示备份的源文件/目录src , $2表示备份的目的目录dest
@@ -31,6 +31,6 @@ scp_fun() {
 # === start scp ==============
 for src in $source
 do
-   scp_fun $src $dest
+  scp_fun $src $dest
 done
  
