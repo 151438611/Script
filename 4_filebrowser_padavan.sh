46c4ea8533d12110d5fc10b9e1071a62ee377983
Jun <33471088+151438611@users.noreply.github.com>
2019-02-24 12:56:52 +0800

    Update 4_filebrowser_padavan.sh

--- 4_filebrowser_padavan.sh
+++ 4_filebrowser_padavan.sh
@@ -1,20 +1,17 @@
 #!/bin/sh
 # for Padavan 
-
 user=$(nvram get http_username) ; cron=/etc/storage/cron/crontabs/$user
 startup=/etc/storage/started_script.sh
 grep -qi $(basename $0) $startup || echo "sh /etc/storage/bin/$(basename $0)" >> $startup
 cron_filebrowser="40 * * * * [ \$(date +%k) -eq 5 ] && killall -q filebrowser ; sleep 8 && sh /etc/storage/bin/$(basename $0)"
 grep -qi $(basename $0) $cron || echo "$cron_filebrowser" >> $cron
 
-# ----- filebrowser_mipsle的下载地址 ------------------------------------
-fb_url=http://opt.cn2qq.com/opt-file/filemanager && md5_fb=957c409aba8623ff6a5b6ed4b8b6045d
-port=2019
-
 udisk=$(mount | awk '/dev/ && /media/ {print $3}' | head -n1)
 [ -z "$udisk" ] && echo "U_disk is not exist , exit !" >> /tmp/filebrowser.log && exit
-
-filebrowser="$udisk/filebrowser" && dir_fb=$(dirname $filebrowser)
+# ----- filebrowser_mipsle的下载地址 ------------------------------------
+fb_url=http://opt.cn2qq.com/opt-file/filemanager && md5_fb=957c409aba8623ff6a5b6ed4b8b6045d
+filebrowser="$udisk/filebrowser" ; port=2019
+dir_fb=$(dirname $filebrowser)
 [ -d "$dir_fb" ] || mkdir -p $dir_fb 
 [ -f "$filebrowser" ] || wget -O $filebrowser $fb_url
 chmod 755 $filebrowser
