ff31367af8088c6085511dc2b991a4ec3377b455
Jun <33471088+151438611@users.noreply.github.com>
2019-02-25 09:16:26 +0800

    Update 3_cronConnectWifi_padavan.sh

--- 3_cronConnectWifi_padavan.sh
+++ 3_cronConnectWifi_padavan.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 # 使用说明: 路由器名称需要包含 k2p/k2/youku ,暂时只支持此型号
 # add crontab,定时强制连接某个指定Wifi，适用于gx5 K2路由器场景
+bin_dir=/etc/storage/bin ; [ -d "$bin_dir" ] || mkdir -p $bin_dir
+startup=/etc/storage/started_script.sh
 cron=/etc/storage/cron/crontabs/$(nvram get http_username)
-grep -qi $(basename $0) $cron || echo "55 5 * * * sh /etc/storage/bin/$(basename $0)" >> $cron
-aplog=/tmp/autoChangeAp.log ; [ -f "$aplog" ] || touch $aplog
+sh_name=$(basename $0) ; sh_url=http://14.116.146.30:11111/file/cronConnectWifi_padavan.sh
+
+grep -qi $sh_name $cron || echo "55 5 * * * sh $bin_dir/$sh_name" >> $cron
+startup_ap="wget -P /tmp $sh_url && mv -f /tmp/$(basename $sh_url) $bin_dir/$sh_name ; sh $bin_dir/$sh_name"
+grep -qi $sh_name $startup || echo "$startup_ap" >> $startup
+aplog=/tmp/autoChangeAp.log
 
 # ===1、设置路由器型号k2p和k2(youku-L1的2.4G接口名为ra0，和k2相同),因为k2和k2p的无线接口名称不一样==========
 host_name=$(nvram get computer_name)
@@ -11,11 +17,12 @@ if [ -n "$(echo $host_name | grep -i k2p)" ] ; then router=k2p
 elif [ -n "$(echo $host_name | grep -Ei "k2|youku")" ] ; then router=k2
 else echo "!!! The router is Unsupported device , exit !!!" >> $aplog && exit
 fi
-# ===2、输入指定被中继的wifi帐号密码,格式{ 无线频段(2|5)+ssid+password+wan_ip },默认加密方式为WPA2PSK/AES===
+# ===2、输入指定被中继的wifi帐号密码,格式{ 无线频段(2|5)+ssid+password+wan_ip },默认加密方式为WPA2-PSK/AES===
 # === 若wifi未加密则password为空，wlan_ip可不填表示wlan动态获取IP
-ap="2+TP-LINK841N+12345678+1"
+apinput=/etc/storage/ez_buttons_script.sh
+ap=$(grep "^[2,5]+" $apinput | head -n1)
 
-band=$(echo $ap | cut -d + -f 1)     ; apssid=$(echo $ap | cut -d + -f 2)
+band=$(echo $ap | cut -d + -f 1)     ; apssid=$(echo $ap | cut -d + -f 2) && [ -z "$apssid" ] && exit
 appasswd=$(echo $ap | cut -d + -f 3) ; gwip=$(echo $ap | cut -d + -f 4)
 rt=$(nvram get rt_mode_x) ; wl=$(nvram get wl_mode_x)
 if   [ $rt -ne 0 -a $wl -eq 0 ] ; then apssid_old=$(nvram get rt_sta_ssid) ; band_old=2
@@ -39,7 +46,7 @@ if [ "$apssid_old" != "$apssid" ] ; then
     sta_ssid=wl_sta_ssid     ; sta_auth_mode=wl_sta_auth_mode      ; sta_wpa_mode=wl_sta_wpa_mode
     sta_crypto=wl_sta_crypto ; sta_wpa_psk=wl_sta_wpa_psk
     nvram set rt_mode_x=0    ; nvram set rt_channel=0
-  else break
+  else exit
   fi
   
   scanwifi ; apinfo=$(echo "$scanlist" | grep "$apssid")
