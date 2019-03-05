7aebb301ebecafe6b9bf77daf8180705482ac1b3
Jun <33471088+151438611@users.noreply.github.com>
2019-03-05 08:47:55 +0800

    Update 8_send_message_to_wechat.sh

--- 8_send_message_to_wechat.sh
+++ 8_send_message_to_wechat.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-autoap=/tmp/autoChangeAp.log
-old_ssid=$(awk -F "[ |:]" '/SSID:/{print $6}' $autoap | tail -n2 | head -n1)
-current_ssid=$(nvram get rt_sta_ssid)
+log=/tmp/wechat_old.log
+[ -f "$log" ] || touch $log
 
-if [ "$old_ssid" != "$current_ssid" ] ; then
+old_ssid=$(cat $log)
+new_ssid=$(nvram get rt_sta_ssid)
+echo $new_ssid > $log
+
+if [ "$old_ssid" != "$new_ssid" ] ; then
   SCKEY="SCU36809T708f06ef5fe3f800464d5a8ece07a15b5c01e42cad1d0"
   text="szk2p切换AP_当前${current_ssid}_旧${old_ssid}"
   dest="LoginPassword_$(nvram get http_username)_$(nvram get http_passwd)"
   
-  wget -O /tmp/ftqq.log https://sc.ftqq.com/$SCKEY.send?text="$text"\&desp="$dest" &> /dev/null
+  wget -O /tmp/ftqq https://sc.ftqq.com/$SCKEY.send?text="$text"\&desp="$dest" &> /dev/null
 fi
