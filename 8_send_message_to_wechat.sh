28872c3e7491dd186025977cc52e2834943a4826
Jun <33471088+151438611@users.noreply.github.com>
2019-03-17 09:50:46 +0800

    Update 8_send_message_to_wechat.sh

--- 8_send_message_to_wechat.sh
+++ 8_send_message_to_wechat.sh
@@ -6,18 +6,13 @@ log=/tmp/wechat_old.log
 
 old=$(cat $log)
 new=$(nvram get rt_sta_ssid)
-
-
 if [ "$old" != "$new" ] ; then
   echo $new > $log
   SCKEY="SCU36809T708f06ef5fe3f800464d5a8ece07a15b5c01e42cad1d0"
   text="szk2pChangeAP_NEW_${new}_OLD_$old"
   dest="LoginPassword_$(nvram get http_username)_$(nvram get http_passwd)"
-  
   wget -O /tmp/ftqq https://sc.ftqq.com/$SCKEY.send?text="$text"\&desp="$dest" &> /dev/null
 fi
-
-
 
 #===================== for armbian_N1 ================================
 #!/bin/bash
@@ -27,13 +22,10 @@ log=/tmp/wechat_old.log
 
 old=$(cat $log)
 new=$(ifconfig eth0 | awk '/inet/ && /netmask/ {print $2}')
-
-
 if [ "$old" != "$new" ] ; then
   echo $new > $log
   SCKEY="SCU36809T708f06ef5fe3f800464d5a8ece07a15b5c01e42cad1d0"
   text="armbian_NEW_${new}_OLD_$old"
   dest="10gtek_armbian_N1_IP_changed"
-  
   wget -O /tmp/ftqq https://sc.ftqq.com/$SCKEY.send?text="$text"\&desp="$dest" &> /dev/null	
 fi
