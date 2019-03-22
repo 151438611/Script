637e94d23d58dedf48e1e8f9d67f317495517502
Jun <33471088+151438611@users.noreply.github.com>
2019-03-22 14:56:30 +0800

    Update 9_etherwake.sh

--- 9_etherwake.sh
+++ 9_etherwake.sh
@@ -5,12 +5,12 @@ grep -qEi "redhat|centos" /etc/os-release && os_type=redhat
 
 case $os_type in
   debian)
-  etherwake=$(which etherwake) 
   [ -n "$etherwake" ] || apt -y install etherwake || exit
+  etherwake=$(which etherwake) 
   ;;
   redhat)
-  etherwake=$(which ether-wake)
   [ -n "$etherwake" ] || yum -y install ether-wake || exit
+  etherwake=$(which ether-wake)
   ;;
 esac
 
