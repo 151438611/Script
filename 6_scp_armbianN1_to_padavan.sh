032147b57f2bf4ed8b23e3107bc54fece1fd8602
Jun <33471088+151438611@users.noreply.github.com>
2019-03-23 14:24:03 +0800

    Update 6_scp_armbianN1_to_padavan.sh

--- 6_scp_armbianN1_to_padavan.sh
+++ 6_scp_armbianN1_to_padavan.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-# 使用scp配合ssh公钥私钥，用来将armbian中的文件同步到jhk2p路由器中保存
-
+# 使用scp配合ssh公钥私钥，用来将armbian中的文件同步到Padavan路由器中保存
+# 默认同步到jhk2p_USB中，使用传参 $1 = youku / szk2p 来同步到youku_TF卡或szk2p_USB中
+router=$1
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin:$PATH
 scplog=/tmp/scp.log ; echo "" >> $scplog
 
@@ -15,20 +16,19 @@ src7=
 src8=
 source="$src0 $src1 $src2 $src3 $src4 $src5 $src6 $src7 $src8"
 
-# ------- scp to jhk2p_75 ----------
+# ------- scp to jhk2p_75_usb ----------
 dest_ip=admin@frp.xiongxinyi.cn
-dest_dir=/media/AiDisk_a2/data
-dest_port=17500
-# ------- scp to youku-L1 ----------
-#dest_dir=/media/AiCard_02/data
-#dest_port=11100
+dest_dir=/media/AiDisk_a2/data && dest_port=17500
+# ------- scp to youku-L1_TF ----------
+[ "${router:=jhk2p}" = youku ] && dest_dir=/media/AiCard_02/data && dest_port=11100
+# ------- scp to szk2p_usb ----------
+[ "$router" = szk2p ] && dest_dir=/media/AiDisk_a1/data && dest_port=17920
 
 dest=$dest_ip:$dest_dir
-
 scp_fun() {
 # $1表示备份的源文件/目录src , $2表示备份的目的目录dest
   scp -C -r -o "StrictHostKeyChecking no" -P ${dest_port:-22} $1 $2
-  [ $? -eq 0 ] && echo "$(date +"%F %T") scp success $1" >> $scplog || echo "$(date +"%F %T") scp fail--- $1" >> $scplog
+  [ $? -eq 0 ] && echo "$(date +"%F %T") scp to $router success $1" >> $scplog || echo "$(date +"%F %T") scp to $router fail--- $1" >> $scplog
 }
 
 # === start scp ==============
