b4909ba7d7601a4301a0dff38e964dd7c8b4b495
Jun <33471088+151438611@users.noreply.github.com>
2019-03-31 01:59:26 +0800

    Update 1_frpc_openwrt.sh

--- 1_frpc_openwrt.sh
+++ 1_frpc_openwrt.sh
@@ -3,7 +3,7 @@
 # 默认frpc连接frps失败10次后,客户端frpc会自动关闭退出;此脚本用于定时检查frpc. 版本: frp_0.22.0
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin:$PATH
 cron=/etc/crontabs/root ; startup=/etc/rc.local ; frpc_name=$(basename $0)
-frpc_sh=http://14.116.146.30:11111/file/frp/frpc_openwrt.sh
+frpc_sh=http://frp.xiongxinyi.cn:11111/file/frp/frpc_openwrt.sh
 if [ $(grep -c $frpc_name $startup) -eq 0 ] ; then
   sed -i /^exit/d $startup
   echo "sleep 40 ; wget -P /tmp/ $frpc_sh && mv -f /tmp/$(basename $frpc_sh) /etc/$frpc_name ; sh /etc/$frpc_name" >> $startup
@@ -22,8 +22,8 @@ lanip=$(uci get network.lan.ipaddr) && i=$(echo $lanip | cut -d . -f 3)
 # ----- 1、填写服务端的IP/域名、认证密码即可 ---------------
 server_addr=frp.xiongxinyi.cn ; token=administrator ; subdomain=$host_name$i
 # ----- 2、frpc的下载地址、frpcini设置临时配置(默认/tmp/重启自动更新)还是永久保存配置(/etc/，需取消注释#) -----
-frpc_url1=http://14.116.146.30:11111/file/frp/frpc_linux_mips && md5_frpc1=756a5320f75b98b12f5bea1846f8d3fa
-frpc_url2=http://14.116.146.30:12222/file/frp/frpc_linux_mips && md5_frpc2=756a5320f75b98b12f5bea1846f8d3fa
+frpc_url1=http://14.116.146.30:11111/file/frp/frpc_linux_mips && md5_frpc1=2bb7a6d32c5f378ba1aede9f669ed37a
+frpc_url2=http://14.116.146.30:12222/file/frp/frpc_linux_mips && md5_frpc2=2bb7a6d32c5f378ba1aede9f669ed37a
 md5_frpc="$md5_frpc1 $md5_frpc2"
 frpc=/tmp/frpc ; frpcini=/etc/frpc.ini 
 
@@ -31,7 +31,7 @@ frpc=/tmp/frpc ; frpcini=/etc/frpc.ini
 download_frpc() {
   rm -f $frpc
   wget -O $frpc $frpc_url1 &
-  sleep 60 ; killall -q frpc wget
+  sleep 100 ; killall -q frpc wget
   [ "$(md5sum $frpc | cut -d " " -f 1)" != "$md5_frpc1" ] && rm -f $frpc && wget -O $frpc $frpc_url2
 }
 frpc_md5sum=$(md5sum $frpc | cut -d " " -f 1)
