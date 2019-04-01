f0caf86d18c43a3c7a8af01eb459e66463d8d596
Jun <33471088+151438611@users.noreply.github.com>
2019-04-01 21:52:02 +0800

    Update 3_cronConnectWifi_padavan.sh

--- 3_cronConnectWifi_padavan.sh
+++ 3_cronConnectWifi_padavan.sh
@@ -6,7 +6,7 @@ export PATH=/bin:/sbin:/usr/bin:/usr/sbin:$PATH
 bin_dir=/etc/storage/bin ; [ -d "$bin_dir" ] || mkdir -p $bin_dir
 startup=/etc/storage/started_script.sh
 cron=/etc/storage/cron/crontabs/$(nvram get http_username)
-sh_name=$(basename $0) ; sh_url=http://14.116.146.30:11111/file/cronConnectWifi_padavan.sh
+sh_name=$(basename $0) ; sh_url=http://frp.xiongxinyi.cn:11111/file/cronConnectWifi_padavan.sh
 grep -qi $sh_name $cron || echo "55 5 * * * sh $bin_dir/$sh_name" >> $cron
 startup_ap="wget -P /tmp $sh_url && mv -f /tmp/$(basename $sh_url) $bin_dir/$sh_name"
 grep -qi $sh_name $startup || echo "$startup_ap" >> $startup
