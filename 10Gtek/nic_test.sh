050a6ea75b5c7b64891713eb5c101170591c1257
Jun <33471088+151438611@users.noreply.github.com>
2019-04-01 13:00:45 +0800

    Update nic_test.sh

--- 10Gtek/nic_test.sh
+++ 10Gtek/nic_test.sh
@@ -16,21 +16,21 @@ read -p "确认测试环境配置是否已完成,默认yes,请输入 <yes/no> : 
 echo -e "\n所有网卡端口号列表:  (state UP表示该端口已链接,state DOWN表示该端口未链接)"
 ip addr | awk '/</ {print $0}'
 read -p "请输入连接的网卡端口号,默认eth1,请输入 <eth1/eth2/eth3/eth4> : " port 
-[ -z "$(echo ${port:=eth1} | grep eth)" ] && echo -e "\n请输入有效的网卡编号，再重新测试!\n" && exit
+port=${port:=eth1} ; [ -z "$(echo $port | grep eth)" ] && echo -e "\n请输入有效的网卡编号，再重新测试!\n" && exit
 echo ""
-read -p "请输入Ping包次数,默认2000次,请输入 <2000-10000> : " count 
-[ -n $(echo ${count:=2000} | tr -d [0-9]) ] && count=2000
+read -p "请输入Ping包次数,默认2000次,请输入 <2000-10000> : " count
+count=${count:=2000} ; [ -n "$(echo $count | tr -d [0-9])" ] && count=2000
 echo ""
 read -p "请输入iperf3性能测试时长,默认60秒,请输入自定义时间,单位为秒 : " iperf_time
-[ -n $(echo ${iperf_time:=60} | tr -d [0-9]) ] && iperf_time=60
+iperf_time=${iperf_time:=60} ; [ -n "$(echo $iperf_time | tr -d [0-9])" ] && iperf_time=60
 
 echo -e "\n开始自动进行测试: "
 
 ethernet=$(lspci | grep -i "Ethernet controller")
 if [ $(echo "$ethernet" | wc -l) -gt 1 ] ; then 
   result="识别网卡成功"
-  echo -e "\n$result" | tee -a $log 
-  echo -e "$ethernet\n" >> $log 
+  echo -e "\n$ethernet" >> $log 
+  echo -e "$result\n" | tee -a $log
 else 
   result="未识别插入的PCI-E网卡"
   echo -e "\n$result,请重新检查是否已插好,再来测试 !!!\n" && exit
@@ -38,21 +38,22 @@ fi
 
 ethtool -i $port >> $log
 [ $? -eq 0 ] && result="读取网卡驱动版本信息成功" || result="读取网卡驱动版本信息失败!"
-echo -e "\n$result" | tee -a $log 
+echo -e "$result\n" | tee -a $log 
 
 ethtool -m $port >> $log
 [ $? -eq 0 ] && result="读取EEPROM信息成功" || result="读取EEPROM信息失败!"
-echo -e "\n$result" | tee -a $log 
+echo -e "$result\n" | tee -a $log 
 
 link_cmd=$(ethtool $port 2> /dev/null)
 link_stat=$(echo "$link_cmd" | awk '/Link detected:/{print $3}')
 link_speed=$(echo "$link_cmd" |awk '/Speed:/{print int($2)}')
 if [ "$link_stat" = "yes" -a -n "link_speed" ] ; then
   result="链路已连通,速率为 $link_speed Mb/s"
-else result="链路连通失败!"
+else 
+  result="链路连通失败!" 
 fi
-echo -e "\n$result" | tee -a $log
-echo "$(ethtool $port)" >> $log 
+[ "$link_stat" = yes ] && echo $link_cmd >> $log
+echo -e "$result\n" | tee -a $log
 
 if [ "$link_stat" = yes ] ; then
   echo -e "\n正在进行 $count 次的 Ping 包测试..."
@@ -72,22 +73,21 @@ if [ "$link_stat" = yes ] ; then
      eth4) dest_ip=192.168.9.101 ;;
     esac
   fi
-  ping -c $count -i 0.05 $dest_ip 2> /dev/null | tee /tmp/ping.log 
-  ping_head=$(head -n6 /tmp/ping.log 2> /dev/null)
-  ping_tail=$(tail /tmp/ping.log 2> /dev/null)
+  ping -c $count -i 0.05 $dest_ip | tee /tmp/ping.log 
+  ping_head=$(head -n6 /tmp/ping.log)
+  ping_tail=$(tail /tmp/ping.log)
   [ -n "$(echo "$ping_tail" | awk '/0% packet loss/ {print $0}')" ] && result="Ping包成功,无丢包." || result="Ping包失败,或有丢包!"
   echo -e "\n$result" | tee -a $log
   echo -e "$ping_head\n......\n$ping_tail" >> $log
 
   echo -e "\n正在进行 $iperf_time 秒的 iperf3 性能测试..."
   iperf3 -c $dest_ip -t 60 | tee /tmp/iperf.log
-  iperf_head=$(head -n6 /tmp/iperf.log 2> /dev/null)
-  iperf_tail=$(tail /tmp/iperf.log 2> /dev/null)
+  iperf_head=$(head -n6 /tmp/iperf.log)
+  iperf_tail=$(tail /tmp/iperf.log)
   [ -n "$(echo "$iperf_tail" | grep "iperf Done")" ] && result="性能测试完成." || result="性能测试失败!" 
   echo -e "\n$result" | tee -a $log
   echo -e "$iperf_head\n......\n$iperf_tail" >> $log 
-
 fi
 
-unix2dos -o $log ; rm -f /tmp/ping.log /tmp/iperf.log
+unix2dos -o $log
 echo -e "\n测试已完成,测试数据保存在 /tmp/nictest.txt ,下次测试会覆盖掉,请及时拷出!!! \n"
