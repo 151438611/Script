75535a82d9121a504addf9a5511d9682791d7de8
Jun <33471088+151438611@users.noreply.github.com>
2019-04-04 16:46:03 +0800

    Update 6_scp_armbianN1_to_padavan.sh

--- 6_scp_armbianN1_to_padavan.sh
+++ 6_scp_armbianN1_to_padavan.sh
@@ -19,11 +19,11 @@ source="$src0 $src1 $src2 $src3 $src4 $src5 $src6 $src7 $src8"
 
 # ------- scp to jhk2p_75_usb ----------
 dest_ip=admin@frp.xiongxinyi.cn
-dest_dir=/media/AiDisk_a2/data && dest_port=17500
+dest_dir=/media/AiDisk_a2/data && dest_port=17500 && frp_dir=/media/AiDisk_a2/frp/
 # ------- scp to youku-L1_TF ----------
-[ "${router:=jhk2p}" = youku ] && dest_dir=/media/AiCard_02/data && dest_port=11100
+[ "${router:=jhk2p}" = youku ] && dest_dir=/media/AiCard_02/data && dest_port=11100 && frp_dir=/media/AiCard_02/frp/
 # ------- scp to szk2p_usb ----------
-[ "$router" = szk2p ] && dest_dir=/media/AiDisk_a1/data && dest_port=17920
+[ "$router" = szk2p ] && dest_dir=/media/AiDisk_a1/data && dest_port=17920  && frp_dir=/media/AiDisk_a1/frp/
 
 dest=$dest_ip:$dest_dir
 scp_fun() {
@@ -37,4 +37,5 @@ for src in $source
 do
   scp_fun $src $dest
 done
- 
+
+scp -C -r -o "StrictHostKeyChecking no" -P ${dest_port:-22} /media/sda1/data/software/frp/frp_windows_for_outside $dest_ip:$frp_dir
