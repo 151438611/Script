87011c14a792a6a2310551394bd32830c968d1e2
Jun <33471088+151438611@users.noreply.github.com>
2019-04-04 16:49:43 +0800

    Update 6_scp_armbianN1_to_padavan.sh

--- 6_scp_armbianN1_to_padavan.sh
+++ 6_scp_armbianN1_to_padavan.sh
@@ -38,7 +38,7 @@ do
   scp_fun $src $dest
 done
 
-frp_backup=/media/sda1/data/software/frp/frp_windows_for_outside
-scp -C -r -o "StrictHostKeyChecking no" -P ${dest_port:-22} $frp_backup $dest_ip:$frp_dir
-[ $? -eq 0 ] && echo "$(date +"%F %T") scp to $router success $frp_backup" >> $scplog || echo "$(date +"%F %T") scp to $router fail--- $frp_backup" >> $scplog
+frp_bak=/media/sda1/data/software/frp/frp_windows_for_outside
+scp -C -r -o "StrictHostKeyChecking no" -P ${dest_port:-22} $frp_bak $dest_ip:$frp_dir
+[ $? -eq 0 ] && echo "$(date +"%F %T") scp to $router success $frp_bak" >> $scplog || echo "$(date +"%F %T") scp to $router fail--- $frp_bak" >> $scplog
 
