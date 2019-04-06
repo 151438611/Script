d9500ef36a29909d7733a75754670bd0aa2d794c
Jun <33471088+151438611@users.noreply.github.com>
2019-04-06 11:50:18 +0800

    Update 5_backupConfig_padavan.sh

--- 5_backupConfig_padavan.sh
+++ 5_backupConfig_padavan.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-# for 定时备份脚本、配置文件到存储目录
+# 备份/etc/storage目录下的脚本文件
 
-cron=/etc/storage/cron/crontabs/$(nvram get http_username)
-bin_dir=/etc/storage/bin
-cp -f $cron $bin_dir/crontab.txt
-
-cd $bin_dir 
-tar -zcvf /tmp/conf_backup_$(nvram get computer_name).tgz *.*
-rm -f $bin_dir/crontab.txt
+cd /etc/storage/
+tar -zcvf /tmp/conf_backup_$(nvram get computer_name).tgz *.sh bin cron
