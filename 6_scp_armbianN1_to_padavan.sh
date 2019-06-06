f07f82d2bd51085800a2af6d7129174e8a65d75b
Jun <33471088+151438611@users.noreply.github.com>
2019-06-06 11:24:11 +0800

    Update 6_scp_armbianN1_to_padavan.sh

--- 6_scp_armbianN1_to_padavan.sh
+++ 6_scp_armbianN1_to_padavan.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 # use scp with ssh id_rsa/id_rsa_pub , copy armbian_disk backup to remote_backup
 # default backup to jhk2p_usb, use $1 = youku / szk2p to youku_tf or szk2p_usb 
-router=$1
-echo -e "\nYou can choose \$1 = jhk2p(default) / youku / szk2p\n"
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin:$PATH
+echo -e "\nuse choose \$1 = jhk2p(default) / youku / szk2p\n"
+
+router=$1 ; router=${router:=jhk2p}
+[ -n "$(echo $router | grep yk)" ] && router=youku
 scplog=/tmp/scp.log ; echo "" >> $scplog
 
 #src0=/media/sda1/data/script
 src1=/media/sda1/data/document
-src2=
+src2=/media/sda1/data/script/Script-master.zip
 src3=
 src4=
 src5=
@@ -21,7 +23,7 @@ source="$src0 $src1 $src2 $src3 $src4 $src5 $src6 $src7 $src8"
 dest_ip=admin@frp.xiongxinyi.cn
 dest_dir=/media/AiDisk_a2/data && dest_port=17500 && frp_dir=/media/AiDisk_a2/frp/
 # ------- scp to youku-L1_TF ----------
-[ "${router:=jhk2p}" = youku ] && dest_dir=/media/AiCard_02/data && dest_port=11100 && frp_dir=/media/AiCard_02/frp/
+[ "$router = youku ] && dest_dir=/media/AiCard_02/data && dest_port=11100 && frp_dir=/media/AiCard_02/frp/
 # ------- scp to szk2p_usb ----------
 [ "$router" = szk2p ] && dest_dir=/media/AiDisk_a1/data && dest_port=17920  && frp_dir=/media/AiDisk_a1/frp/
 
