009e588ee159bf9b713b6e08f01900ca0192c563
Jun <33471088+151438611@users.noreply.github.com>
2019-07-30 09:08:33 +0800

    Update 1_frp_linux_base.sh

--- 1_frp_linux_base.sh
+++ 1_frp_linux_base.sh
@@ -8,7 +8,7 @@ frpini=/opt/frpc/frpc.ini
 
 # ------for padavan -------------------------
 #udisk=$(mount | awk '$1~"/dev/" && $3~"/media/"{print $3}' | head -n1)
-#[ -z "$udisk" ] && echo "$(date +"%F %T") frp is no exist !" && exit
+#[ -z "$udisk" ] && echo "$(date +"%F %T") frp is no exist !" && exit 1
 #frp=${udisk:=/tmp}/frpc
 #frpini=/etc/storage/bin/frpc.ini
 
