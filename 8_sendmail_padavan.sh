cdba4147223b0b89c3e8555c1f060d2d616fdafe
Jun <33471088+151438611@users.noreply.github.com>
2019-07-30 12:42:10 +0800

    Update 8_sendmail_padavan.sh

--- 8_sendmail_padavan.sh
+++ 8_sendmail_padavan.sh
@@ -12,10 +12,14 @@ smtp_add=smtp.${from_add#*@}
 to_add=xiongjun0928@foxmail.com
 cc_add=jun_xiong@10gsfp.com
 subject="$(date +%F)---Hostname---$(nvram get computer_name)"
+
+frpc=$(ps | grep frpc | grep -v grep | awk '{print $5,$6,$7}')
 message="$(nvram get http_username) / $(nvram get http_passwd) / uptime---$(uptime)
 $(free | head -n2)
 
-$(ifconfig | awk 'BEGIN{print "Iface_IP infomation : "}/inet addr/ || /HWaddr/ {print $0}')
+#$(ifconfig | awk 'BEGIN{print "Iface_IP infomation : "}/inet addr/ || /HWaddr/ {print $0}')
+
+$($frpc status)
 
 $(awk -F, 'BEGIN{print "Client infomation : "} {print $1"\t"$2"\t"$3}' /tmp/static_ip.inf)
 
@@ -23,8 +27,8 @@ $(awk -F, 'BEGIN{print "Client infomation : "} {print $1"\t"$2"\t"$3}' /tmp/stat
 autoChangeAp.log : 
 $(tail -n 48 /tmp/autoChangeAp.log | grep -E ":00:|WIFI")
 "
-mailtxt=/tmp/mail.txt
 
+mailtxt=/tmp/mail.txt
 cat << END > $mailtxt
 From:$from_add
 To:$to_add
