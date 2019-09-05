f614f5ae9f76a7cf20be4ee5319ec610076b8989
Jun <33471088+151438611@users.noreply.github.com>
2019-09-05 13:51:03 +0800

    Update 1_frpc_padavan.sh

--- 1_frpc_padavan.sh
+++ 1_frpc_padavan.sh
@@ -23,9 +23,9 @@ sh_url=${download_url}frp/frpc_padavan.sh
 
 cron_reboot="5 5 * * * [ -n \"\$(date +%d | grep 5)\" ] && reboot || ping -c2 -w5 114.114.114.114 || reboot"
 grep -qi "reboot" $cron || echo "$cron_reboot" >> $cron
-cron_sh="20 * * * * /bin/bash $bin_dir/$sh_name"
+cron_sh="20 * * * * sh $bin_dir/$sh_name"
 grep -qi $sh_name $cron || echo "$cron_sh" >> $cron
-startup_sh="sleep 30 ; wget -P /tmp $sh_url && mv -f /tmp/$(basename $sh_url) $bin_dir/$sh_name ; /bin/bash $bin_dir/$sh_name"
+startup_sh="sleep 30 ; wget -P /tmp $sh_url && mv -f /tmp/$(basename $sh_url) $bin_dir/$sh_name ; sh $bin_dir/$sh_name"
 grep -qi $sh_name $startup || echo "$startup_sh" >> $startup
 
 # 开启从wan口访问路由器和ssh服务(默认关闭)，即从上级路由直接访问下级路由或ssh服务
