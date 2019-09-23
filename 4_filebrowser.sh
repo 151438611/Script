62ab9080955e4eb4da355acfec7b7fd276a709d0
Jun <33471088+151438611@users.noreply.github.com>
2019-09-23 15:08:42 +0800

    Update and rename 4_filebrowser_padavan.sh to 4_filebrowser.sh

---
+++ 4_filebrowser.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# for Padavan ,support filebrowser 2.0.x
+# FileBrowser v2.0.x 使用方法
+#1 ./filebrowser -d ./filebrowser.db config init  初始化数据库配置文件
+#2 ./filebrowser -d ./filebrowser.db config set -p 2019 -l /tmp/filebrowser.log
+#3 ./filebrowser -d ./filebrowser.db users add username passwd --perm.admin  添加管理员帐号
+#4 ./filebrowser -d ./filebrowser.db &  后台启动软件
+
+# 请输入完整路径
+exePath="/opt/filebrowser" && exeName=${exePath##*/}
+confPath=""
+exeCommand="$exePath -a 0.0.0.0 -d /opt/filebrowser.db -p 8080 -l /tmp/filebrowser.log"
+
+log="/tmp/${exeName}.log"
+
+cd $(dirname $exePath)
+# 判断执行文件是否存在，且有执行权限
+if [ -f "$exePath" -a -n "$exePath" ]; then
+	[ -x $exePath ] || chmod +x $exePath
+else
+	echo "$(date +"%F %T") $exePath file does not exist ! ! !" >> $log 
+	exit
+fi	
+
+if [ -n "$confPath" -a ! -f $confPath ];then
+	echo "$(date +"%F %T") $confPath file does not exist ! ! !" >> $log
+	exit
+fi
+# 判断执行进程是否存在
+if [ -n "$(pidof $exeName)" ]; then
+	echo "$(date +"%F %T") $exeName is runing, Don't do anything !" >> $log
+else 
+	echo "$(date +"%F %T") $exeName was not runing ; start $exeName ... " >> $log
+	exec $exeCommand &
+fi
