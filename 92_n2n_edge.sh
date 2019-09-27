bd8e16715dc71c646c780bb9c0e3c6e8b6290127
Jun <33471088+151438611@users.noreply.github.com>
2019-09-27 16:30:46 +0800

    Add files via upload

--- 92_n2n_edge.sh
+++ 92_n2n_edge.sh
@@ -9,7 +9,7 @@ vmnic_name=n2n_edge
 community_name=n2n
 ipadd=10.0.0.x
 netmask=255.255.255.0
-# 是否加密(加密后仅密码一致的节点可互相通信),会影响速度 --- 不建议使用
+# 是否加密(加密后仅密码一致的节点可互相通信) --- 会影响速度，不建议使用此选项
 N2N_KEY=	
 
 log=/tmp/n2n_log.txt
@@ -37,6 +37,11 @@ case $hw_type in
 	;;
 esac
 
+addIptables() {
+	[ -z "$(iptables -vnL INPUT | grep $vmnic_name)" ] && \
+		iptables -A INPUT -i $vmnic_name -j ACCEPT
+}
+
 if [ ! -x $edge ]; then
 	rm -f $edge
 	wget -O $edge $down_url
@@ -47,6 +52,7 @@ ping -c 2 -w 3 114.114.114.114 && \
 if [ -n "$(pidof ${edge##*/})" ]; then
 	echo "$(date +"%F %T")	${edge##*/} $ipadd is runing , Don't do anything !" >> $log
 else
+	addIptables
 	echo "$(date +"%F %T")	${edge##*/} $ipadd was not runing ; start ${edge##*/} ..." >> $log
 	[ $N2N_KEY ] && \
 	exec $edge -r -d $vmnic_name -c $community_name -a $ipadd -s $netmask -l $supernode_ip_port -k $N2N_KEY || \
