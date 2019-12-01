22b216b575553c4c411563d3790eea620ff62663
Jun <33471088+151438611@users.noreply.github.com>
2019-12-01 13:42:06 +0800

    Update 2_autoChangeAp_padavan.sh

--- 2_autoChangeAp_padavan.sh
+++ 2_autoChangeAp_padavan.sh
@@ -7,7 +7,7 @@ bin_dir=/etc/storage/bin ; [ -d "$bin_dir" ] || mkdir -p $bin_dir
 startup=/etc/storage/started_script.sh
 cron=/etc/storage/cron/crontabs/$(nvram get http_username)
 sh_name=$(basename $0)
-main_url="http://frp.xiongxinyi.cn:30100/file/"
+main_url="http://frp.xxy1.ltd:35100/file/"
 sh_url="${main_url}autoChangeAp_padavan.sh"
 grep -qi $sh_name $cron || echo "*/30 * * * * sh $bin_dir/$sh_name" >> $cron
 startup_ap="sleep 30 ; wget -P /tmp $sh_url && mv -f /tmp/$(basename $sh_url) $bin_dir/$sh_name ; sh $bin_dir/$sh_name"
