d6b5472cadcc2e28c268d2099f6367be0b1db3b4
Jun <33471088+151438611@users.noreply.github.com>
2020-01-04 09:42:30 +0800

    Update and rename Ping_Delay.sh to ping_delay.sh

--- zabbix/ping_delay.sh
+++ zabbix/ping_delay.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # 适用Zabbix-agent(support x86_64 and arm64)自定义脚本，用于检测ping目标主机的延时; 时间单位:ms
 # $1表示目录主机的 IP 或 域名
-# 注意: 需要修改 zabbix_server.conf 和 zabbix_agentd.conf 中的 Timeout=10 或更长
+# 注意: 
+#  1 需要修改 zabbix_server.conf 和 zabbix_agentd.conf 中的 Timeout=10 或更长
+#  2 chmod +x ping_delay.sh
+# zabbix_agentd.conf: UserParameter=ping.delay[*],/bin/bash /etc/zabbix/ping_delay.sh $1
 
 #export PATH=/bin:/sbin:/usr/bin:/usr/sbin:$PATH
 
