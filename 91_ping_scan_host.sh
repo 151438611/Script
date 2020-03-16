317e4e925586af2ab64f14119b07350d239b88dc
Jun <33471088+151438611@users.noreply.github.com>
2020-03-16 15:05:02 +0800

    Update 91_ping_scan_host.sh

--- 91_ping_scan_host.sh
+++ 91_ping_scan_host.sh
@@ -1,23 +1,56 @@
 #!/bin/sh
-# support all Linux 
-# 
+# 适用于Linux中没有类似fping的批量ping网段的功能，使用脚本来实现
+# 暂时只支持子网掩码为255.255.255.0的网段
+# 使用操作： sh ping_scan_host.sh start_ip end_host
 
-subnet=192.168.1.
-hosts=/tmp/scan_host.log ; echo -e "$(date +"%F %T") start scan ${subnet}x ...... \n" > $hosts
+start_ip=$1
+end_host=$2
 
-a=$1 ; b=$2
-[ -n "$(echo $a | tr -d [0-9])" -o -n "$(echo $b | tr -d [0-9])" -o -z "$a" ] && echo "please input subnet_number" && exit
-if [ "$a" = 0 ] ; then unset a
-  [ -z "$b" -o "$b" = 0 ] && echo "please input not zero subnet_number" && exit
-elif [ "$b" = 0 ] ; then unset b
-elif [ -n "$b" ] ; then 
-  [ "$a" -gt "$b" ] && c=$a && a=$b && b=$c
-  [ "$b" -ge 255 ] && echo "It's Max subnet_number cann't grate than 255" && exit
+checkIPAddr_fun() {
+	# 检查传入的2个参数IP和主机号是否合规
+	ipaddr=$1
+	host_num=$2
+	if [ -n "$(echo "${ipaddr}${host_num}" | tr -d [0-9] | grep ...)" ] ; then
+		if [ $(echo "$ipaddr" | sed 's/\./ /g' | wc -w) -eq 4 ] ; then
+			nums=$(echo "${ipaddr} ${host_num}" | sed 's/\./ /g')
+			[[ $(echo "$nums" | cut -d " " -f 1) -eq 0  || $host_num -eq 0 ]] && {
+				echo "START_IP or END_HOST input error , First_HOST_NUM can't not Zero !!!"
+				return 3
+				}
+			for a in $nums
+			do
+				[ $a -ge 255 ] && echo "START_IP or END_HOST input error , Cann't grate than 255 !!!" && return 3
+			done
+		else
+			echo "START_IP or END_HOST input error 2 !!!"
+			return 2
+		fi
+	else
+		echo "START_IP or END_HOST input error 1 !!!"
+		return 1
+	fi
+	return 0
+}
+if [ $# -eq 2 ] ; then
+	checkIPAddr_fun $start_ip $end_host
+	[ $? -eq 0 ] || exit 1
+
+	start_ip_net=${start_ip%.*}.
+	start_ip_end=${start_ip##*.}
+	[ $start_ip_end -gt $end_host ] && {
+		temp=$start_ip_end
+		start_ip_end=$end_host
+		end_host=$temp
+		}
+else
+	echo -e "USE: bash $0 START_IP END_HOST \nExample: bash ping_scan_host 192.168.1.1 255"
+	exit 1
 fi
-[ "$a" -ge 255 ] && echo "It's Min subnet_number cann't grate than 255" && exit
 
-for x in `seq $a $b`
+log=/tmp/ping_scan_host.log
+echo "$(date +"%F %T") start scan ${start_ip_net}x ... " > $log
+
+for x in $(seq $start_ip_end $end_host)
 do
-  ping -w 1 $subnet$x && \
-  echo "$subnet$x MAC: $(arp -an | awk -F [\(\)] '$2=="'$subnet$x'" {print $0}')" >> $hosts
+  ping -w 1 ${start_ip_net}$x && echo "$(arp -an | grep ${start_ip_net}$x)" >> $log
 done
