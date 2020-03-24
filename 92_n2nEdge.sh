507356536c75602ce74f068ee950b70bf3564b03
Jun <33471088+151438611@users.noreply.github.com>
2020-03-24 21:14:32 +0800

    Update 92_n2nEdge.sh

--- 92_n2nEdge.sh
+++ 92_n2nEdge.sh
@@ -77,8 +77,8 @@ addIptables() {
 	[ -z "$(iptables -vnL FORWARD | grep $vmnic_name)" ] && \
 	iptables -A FORWARD -i $vmnic_name -j ACCEPT
 	
-	# 适用于 szK2p_20
-	#[ -z "$(iptables -t nat -vnL POSTROUTING | grep -Ei "${vmnic_name}|${ipadd}" ] && \
+	# 适用于 szK2P_20
+	#[ -z "$(iptables -t nat -vnL POSTROUTING | grep -Ei "${vmnic_name}|${ipadd}")" ] && \
 	#iptables -t nat -A POSTROUTING -o $vmnic_name -d ${ipadd%.*}.0/24 -j SNAT --to-source $ipadd
 }
 
