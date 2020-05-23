0def191c8ae84ab86481a3f2e6a6ba6db8f86ade
Jun <33471088+151438611@users.noreply.github.com>
2020-05-23 13:25:34 +0800

    Add files via upload

--- 4_rdesktop.sh
+++ 4_rdesktop.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-# "Usage: bash rdesktop.sh User Host"
+# Install : apt install rdesktop
+# Usage Eamples : bash rdesktop.sh User Host
 
 echo "Usage: bash rdesktop.sh User Host"
-echo "if Input User/Host is Empty, then User/Host is Default;"
+echo "If Input User/Host is Empty, then User/Host is Default ;"
 
-host=192.168.20.22
-rpd_user=Jun
-rpd_passwd=
+HOST=192.168.20.22
+USERNAME=Jun
+PASSWORD=
 
-[ $1 ] && rpd_user=$1
-[ $2 ] && host=$2
+[ $1 ] && USERNAME=$1
+[ $2 ] && HOST=$2
 
-[ -z "$host" -a -z "$rpd_user" ] && echo "RPC User or Host_IP not is Empty" && exit 2
+[ -z "$HOST" -a -z "$USERNAME" ] && echo "RPC Login User or Host not is Empty" && exit 2
 
 option="-g 1280x960 -r disk:opt_share=/opt -r sound:local -r clipboard:PRIMARYCLIPBOARD -P -x l"
 
 #option="-f -r disk:opt_share=/opt -r sound:local -r clipboard:PRIMARYCLIPBOARD -P -x l"
 
 [ "$(pidof rdesktop)" ] || \
-	exec rdesktop -u $rpd_user -p "$rpd_passwd" $option $host
+	exec rdesktop -u $USERNAME -p "$PASSWORD" $option $HOST
