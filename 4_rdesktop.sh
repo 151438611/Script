f8854e46bb46aca1d8ebf39e8485c4c2283f017a
Jun <33471088+151438611@users.noreply.github.com>
2020-05-23 13:51:53 +0800

    Add files via upload

--- 4_rdesktop.sh
+++ 4_rdesktop.sh
@@ -2,8 +2,7 @@
 # Install : apt install rdesktop
 # Usage Eamples : bash rdesktop.sh User Host
 
-echo "Usage: bash rdesktop.sh User Host"
-echo "If Input User/Host is Empty, then User/Host is Default ;"
+echo -e "Usage: bash rdesktop.sh User Host \nIf Input User/Host is Empty, then User/Host is Default ;\n"
 
 HOST=192.168.20.22
 USERNAME=Jun
@@ -21,5 +20,8 @@ option="-g 1280x960 -r disk:opt_share=/opt -r sound:local -r clipboard:PRIMARYCL
 RPC=$(which rdesktop)
 [ $RPC ] || { echo "rdesktop : command not found; Please install the command first !" && exit 2; } 
 
-[ "$(pidof rdesktop)" ] || \
-	exec $RPC -u $USERNAME -p "$PASSWORD" $option $HOST
+if [ -n "$(pidof rdesktop)" ]; then
+	[ "$(ps a | grep $HOST)" ] && echo "The $HOST computer is under remote control !"
+else
+	exec $RPC -u $USERNAME -p "$PASSWORD" $option $HOST & 
+fi
