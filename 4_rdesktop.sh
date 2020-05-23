f8cb85619ebb2a6276114381472175f52238ddad
Jun <33471088+151438611@users.noreply.github.com>
2020-05-23 20:17:44 +0800

    Update 4_rdesktop.sh

--- 4_rdesktop.sh
+++ 4_rdesktop.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 # Install : apt install rdesktop
-# Usage Eamples : bash rdesktop.sh $1_Host $2_User $3_Screen_resolution
+# Usage Eamples : bash rdesktop.sh $1_Host $2_User $3_Password
 
-echo -e "Usage: bash rdesktop.sh \$1_Host \$2_User \$3_Screen_resolution \nIf Input User/Host is Empty, then User/Host is Default ;\n"
+echo -e "Usage: bash rdesktop.sh \$1_Host \$2_User \$3_Password \nIf Input User/Host is Empty, then User/Host is Default ;\n"
 
 HOST=192.168.20.22
 USERNAME=Jun
 PASSWORD=
-GEOMETRY=1280x960
 
 [ $1 ] && HOST=$1
 [ $2 ] && USERNAME=$2
-[ $3 ] && GEOMETRY=$3
+[ $3 ] && PASSWORD=$3
 
 [ -z "$HOST" -a -z "$USERNAME" ] && echo "RPC Login User or Host not is Empty" && exit 2
 
-option="-g $GEOMETRY -r disk:opt_share=/opt -r sound:local -r clipboard:PRIMARYCLIPBOARD -P -x l -z"
-#option="-f -r disk:opt_share=/opt -r sound:local -r clipboard:PRIMARYCLIPBOARD -P -x l -z"
+#option="-g $GEOMETRY -r disk:tmp_share=/tmp -r sound:local -r clipboard:PRIMARYCLIPBOARD -P -x l -z"
+option="-f -r disk:tmp_share=/tmp -r sound:local -r clipboard:PRIMARYCLIPBOARD -P -x l -z"
 
 RPC=$(which rdesktop)
 [ $RPC ] || { echo "rdesktop : command not found; Please install the command first !" && exit 2; } 
