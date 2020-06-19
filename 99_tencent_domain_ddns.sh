e934d683d4630cac23fd38c688c06b2569c5d1f9
Jun <33471088+151438611@users.noreply.github.com>
2020-06-20 00:16:36 +0800

    Update 99_tencent_domain_ddns.sh

--- 99_tencent_domain_ddns.sh
+++ 99_tencent_domain_ddns.sh
@@ -1,51 +1,42 @@
 #!/bin/bash
 # tencent domain ddns , Only support x86_64/arm64 Linux
 
-domain=xxy1.ltd
-subDomain=test1
+Domain=xxy1.ltd
+SubDomain=test
 # ===== Change SecretId / SecretKey ===========
-sId=""
-sKey=""
+SecretId="xx"
+SecretKey="xx"
 # =============================================
+Timestamp=$(date +%s)
+Nonce=$(head -n 8 /dev/urandom | tr -cd 0-9 | head -c 5)
+SignatureMethod=HmacSHA1
+URL="https://cns.api.qcloud.com/v2/index.php"
 
-timestamp=$(date +%s)
-nonce=$(head -n 8 /dev/urandom | tr -cd 0-9 | head -c 5)
-signatureMethod=HmacSHA1
-url="https://cns.api.qcloud.com/v2/index.php"
-
-getRecordId() {
-	action=RecordList
-	
-	src=$(printf "GETcns.api.qcloud.com/v2/index.php?Action=%s&Nonce=%s&SecretId=%s&SignatureMethod=%s&Timestamp=%s&domain=%s" $action $nonce $sId $signatureMethod $timestamp $domain)
-	signature=$(echo -n $src | openssl dgst -sha1 -hmac $sKey -binary | base64)
-
-	params=$(printf "Action=%s&domain=%s&Nonce=%s&SecretId=%s&Signature=%s&SignatureMethod=%s&Timestamp=%s" $action $domain $nonce $sId "$signature" $signatureMethod $timestamp)
-
-	
-	recordId=$(curl -G -d "$params" --data-urlencode "Signature=$signature" "$url" | \
-	grep "$subDomain" | awk -F \"name\":\""'$subDomain'"\" '{print $1}' | awk -F \{ '{print $NF}' | awk -F \, '{print $1}' | tr -cd 0-9)
+getRecordID() {
+	Action=RecordList
+	SRC=$(printf "GETcns.api.qcloud.com/v2/index.php?Action=%s&Nonce=%s&SecretId=%s&SignatureMethod=%s&Timestamp=%s&domain=%s" $Action $Nonce $SecretId $SignatureMethod $Timestamp $Domain)
+	Signature=$(echo -n $SRC | openssl dgst -sha1 -hmac $SecretKey -binary | base64)
+	Params=$(printf "Action=%s&domain=%s&Nonce=%s&SecretId=%s&Signature=%s&SignatureMethod=%s&Timestamp=%s" $Action $Domain $Nonce $SecretId "$Signature" $SignatureMethod $Timestamp)
+	AllRecord=$(curl -G -d "$Params" --data-urlencode "Signature=$Signature" "$URL" | sed 's/{/\n/g' | grep \"name\":\"$SubDomain\")
+	RecordID=$(echo $AllRecord | awk -F [:,] '{print $2}')
+	RecordIP=$(echo $AllRecord | awk -F [:,\"] '{print $13}')
 }
-
-getRecordId
-[ -z "$recordId" ] && echo "Get recordId Fail !!!" && exit 1
+getRecordID
+[ -z "$RecordID" -o -z "$RecordIP" ] && echo "Get Record ID or IP Fail !!!" && exit 1
 
 changeRecordModify() {
-	
-	action=RecordModify
-	recordType=A
-	recordLine='默认'
+	Action=RecordModify
+	RecordType=A
+	RecordLine='默认'
 	
 	# get public ip address
-	getIP=$(curl -q http://ip.sb)
-	value=$getIP
-
-	src=$(printf "GETcns.api.qcloud.com/v2/index.php?Action=%s&Nonce=%s&SecretId=%s&SignatureMethod=%s&Timestamp=%s&domain=%s&recordId=%s&recordLine=%s&recordType=%s&subDomain=%s&value=%s" $action $nonce $sId $signatureMethod $timestamp $domain $recordId $recordLine $recordType $subDomain $value)
-
-	signature=$(echo -n $src | openssl dgst -sha1 -hmac $sKey -binary | base64)
-
-	params=`printf "Action=%s&Nonce=%s&SecretId=%s&SignatureMethod=%s&Timestamp=%s&domain=%s&recordId=%s&recordLine=%s&recordType=%s&subDomain=%s&value=%s" $action $nonce $sId $signatureMethod $timestamp $domain $recordId $recordLine $recordType $subDomain $value`
-
-	curl -G -d "$params" --data-urlencode "Signature=$signature" "$url"
+	getPublicIP=$(curl -q http://ip.sb)
+	RecordValue=$getPublicIP
+    if [ $RecordValue != $RecordIP ]; then
+    	SRC=$(printf "GETcns.api.qcloud.com/v2/index.php?Action=%s&Nonce=%s&SecretId=%s&SignatureMethod=%s&Timestamp=%s&domain=%s&recordId=%s&recordLine=%s&recordType=%s&subDomain=%s&value=%s" $Action $Nonce $SecretId $SignatureMethod $Timestamp $Domain $RecordID $RecordLine $RecordType $SubDomain $RecordValue)
+    	Signature=$(echo -n $SRC | openssl dgst -sha1 -hmac $SecretKey -binary | base64)
+    	Params=`printf "Action=%s&Nonce=%s&SecretId=%s&SignatureMethod=%s&Timestamp=%s&domain=%s&recordId=%s&recordLine=%s&recordType=%s&subDomain=%s&value=%s" $Action $Nonce $SecretId $SignatureMethod $Timestamp $Domain $RecordID $RecordLine $RecordType $SubDomain $RecordValue`
+    	curl -G -d "$Params" --data-urlencode "Signature=$Signature" "$URL"
+	fi
 }
-
 changeRecordModify
