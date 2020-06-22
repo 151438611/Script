aada651fd19d0f7a626965c54c79907bda3cb884
Jun <33471088+151438611@users.noreply.github.com>
2020-06-22 09:38:53 +0800

    Update 99_tencent_domain_ddns.sh

--- 99_tencent_domain_ddns.sh
+++ 99_tencent_domain_ddns.sh
@@ -15,9 +15,11 @@ Log=/tmp/ddns.log
 
 getRecordID() {
 	Action=RecordList
-	SRC=$(printf "GETcns.api.qcloud.com/v2/index.php?Action=%s&Nonce=%s&SecretId=%s&SignatureMethod=%s&Timestamp=%s&domain=%s" $Action $Nonce $SecretId $SignatureMethod $Timestamp $Domain)
+	SRC=$(printf "GETcns.api.qcloud.com/v2/index.php?Action=%s&Nonce=%s&SecretId=%s&SignatureMethod=%s&Timestamp=%s&domain=%s" \
+		$Action $Nonce $SecretId $SignatureMethod $Timestamp $Domain)
 	Signature=$(echo -n $SRC | openssl dgst -sha1 -hmac $SecretKey -binary | base64)
-	Params=$(printf "Action=%s&domain=%s&Nonce=%s&SecretId=%s&Signature=%s&SignatureMethod=%s&Timestamp=%s" $Action $Domain $Nonce $SecretId "$Signature" $SignatureMethod $Timestamp)
+	Params=$(printf "Action=%s&domain=%s&Nonce=%s&SecretId=%s&Signature=%s&SignatureMethod=%s&Timestamp=%s" \
+		$Action $Domain $Nonce $SecretId "$Signature" $SignatureMethod $Timestamp)
 	AllRecord=$(curl -G -d "$Params" --data-urlencode "Signature=$Signature" "$URL" | sed 's/{/\n/g' | grep \"name\":\"$SubDomain\")
 	RecordID=$(echo $AllRecord | awk -F [:,] '{print $2}')
 	RecordIP=$(echo $AllRecord | awk -F [:,\"] '{print $13}')
@@ -36,9 +38,11 @@ changeRecordModify() {
     	if [ $RecordValue = $RecordIP ]; then
 		echo "$(date +"%F %T") The Record_IP($RecordIP) is same as Public_IP($RecordValue) ." >> $Log
 	else
-		SRC=$(printf "GETcns.api.qcloud.com/v2/index.php?Action=%s&Nonce=%s&SecretId=%s&SignatureMethod=%s&Timestamp=%s&domain=%s&recordId=%s&recordLine=%s&recordType=%s&subDomain=%s&value=%s" $Action $Nonce $SecretId $SignatureMethod $Timestamp $Domain $RecordID $RecordLine $RecordType $SubDomain $RecordValue)
+		SRC=$(printf "GETcns.api.qcloud.com/v2/index.php?Action=%s&Nonce=%s&SecretId=%s&SignatureMethod=%s&Timestamp=%s&domain=%s&recordId=%s&recordLine=%s&recordType=%s&subDomain=%s&value=%s" \
+			$Action $Nonce $SecretId $SignatureMethod $Timestamp $Domain $RecordID $RecordLine $RecordType $SubDomain $RecordValue)
 		Signature=$(echo -n $SRC | openssl dgst -sha1 -hmac $SecretKey -binary | base64)
-		Params=$(printf "Action=%s&Nonce=%s&SecretId=%s&SignatureMethod=%s&Timestamp=%s&domain=%s&recordId=%s&recordLine=%s&recordType=%s&subDomain=%s&value=%s" $Action $Nonce $SecretId $SignatureMethod $Timestamp $Domain $RecordID $RecordLine $RecordType $SubDomain $RecordValue)
+		Params=$(printf "Action=%s&Nonce=%s&SecretId=%s&SignatureMethod=%s&Timestamp=%s&domain=%s&recordId=%s&recordLine=%s&recordType=%s&subDomain=%s&value=%s" \
+			$Action $Nonce $SecretId $SignatureMethod $Timestamp $Domain $RecordID $RecordLine $RecordType $SubDomain $RecordValue)
 		curl -G -d "$Params" --data-urlencode "Signature=$Signature" "$URL"
 		[ $? -eq 0 ] && echo "$(date +"%F %T") The Record_IP($RecordIP) is different as Public_IP($RecordValue) , changeRecordModify success !" >> $Log || \
 			echo "$(date +"%F %T") The Record_IP($RecordIP) is different as Public_IP($RecordValue) , but changeRecordModify fail !!!" >> $Log 
