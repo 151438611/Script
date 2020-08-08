39405b1d8c66e275310d5a9756168878693a0b11
Jun <33471088+151438611@users.noreply.github.com>
2020-08-08 10:16:10 +0800

    Update 4_rdesktop.sh

--- 4_rdesktop.sh
+++ 4_rdesktop.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Install : apt install rdesktop
 # Usage Eamples : bash rdesktop.sh $1_Host $2_User $3_Password $4_OPTION(user "Option1 Option2 ...")
-# Don't to user: ./rdesktop.sh $1 $2 $3 ; can't work
 # user keyboard: "Ctrl + Alt + Enter" to Full_Screen or Exit Full_Screen
 
 echo "Usage: bash rdesktop.sh \$1_Host \$2_User \$3_Password "
@@ -10,7 +9,7 @@ HOST=10.5.5.51
 USERNAME=Jun
 PASSWORD=
 #OPTION="-g 1280x960 -P -x l -z -r disk:share=/tmp -r sound:local -r clipboard:PRIMARYCLIPBOARD"
-OPTION="-P -f -r disk:share=/tmp -r sound:local -r clipboard:off"
+OPTION="-P -f -x l -r disk:share=/tmp -r sound:local -r clipboard:off"
 
 [ $1 ] && HOST=$1
 [ $2 ] && USERNAME=$2
@@ -28,7 +27,7 @@ OPTION="-P -f -r disk:share=/tmp -r sound:local -r clipboard:off"
 RPC=$(which rdesktop)
 [ $RPC ] || { echo "rdesktop : command not found; Please install the command first !" && exit 2; } 
 
-if [ -n "$(pgrep -a rdesktop | grep $HOST)" ]; then
+if [ -n "$(pidof rdesktop)" ]; then
 	echo "The $HOST computer is under remote control !" 
 else
 	exec $RPC $HOST -u $USERNAME -p "$PASSWORD" $OPTION $OPTION_O &
