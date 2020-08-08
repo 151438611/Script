b6cbd9b905e4d16d970f0c61a5203519e66a8b4e
Jun <33471088+151438611@users.noreply.github.com>
2020-08-08 10:07:12 +0800

    Update 4_rdesktop.sh

--- 4_rdesktop.sh
+++ 4_rdesktop.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 # Install : apt install rdesktop
 # Usage Eamples : bash rdesktop.sh $1_Host $2_User $3_Password $4_OPTION(user "Option1 Option2 ...")
-# 
+# Don't to user: ./rdesktop.sh $1 $2 $3 ; can't work
+# user keyboard: "Ctrl + Alt + Enter" to Full_Screen or Exit Full_Screen
 
 echo "Usage: bash rdesktop.sh \$1_Host \$2_User \$3_Password "
 
-HOST=192.168.20.22
+HOST=10.5.5.51
 USERNAME=Jun
 PASSWORD=
 #OPTION="-g 1280x960 -P -x l -z -r disk:share=/tmp -r sound:local -r clipboard:PRIMARYCLIPBOARD"
-OPTION="-P -r disk:share=/tmp -r sound:local -r clipboard:CLIPBOARD"
+OPTION="-P -f -r disk:share=/tmp -r sound:local -r clipboard:off"
 
 [ $1 ] && HOST=$1
 [ $2 ] && USERNAME=$2
