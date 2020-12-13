2c7f4efabcc7c40e3f3e938b3417ec7a3e84ed55
Jun <33471088+151438611@users.noreply.github.com>
2020-12-13 12:57:37 +0800

    Add files via upload

---
+++ xianyu_Customer/Only_Allow_OneUser_Login_System.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# disable someone user to login system
+
+
+while :
+do
+
+	user_all=$(w | awk '/pts/{print $1,$2}' | grep -v root)
+	first_user=$(echo "$user_all" | awk 'NR==1 {print $1}')
+	other_user_tty=$(echo "$user_all" | grep -v $first_user | awk '{print $2}')
+	[ "$other_user_tty" ] && {
+		for tty in $other_user_tty
+		do
+			pkill -kill -t $tty
+		done
+		}
+		sleep 3
+	
+done
