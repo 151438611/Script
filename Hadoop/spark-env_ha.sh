26b6f7f686ede62a548eb9f2ada5119527f7036a
Jun <33471088+151438611@users.noreply.github.com>
2020-12-21 11:16:24 +0800

    Update spark-env_ha.sh

--- Hadoop/spark-env_ha.sh
+++ Hadoop/spark-env_ha.sh
@@ -1,7 +1,9 @@
 # spark-3.0.1-bin-hadoop2.7
 
 export JAVA_HOME=/usr/local/java
-#export SPARK_MASTER_IP=master1
+#export SPARK_MASTER_HOST=master
+export SPARK_MASTER_PORT=7077
+export SPARK_MASTER_WEBUI_PORT=8080
 export HADOOP_HOME=/usr/local/hadoop
 export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
 export SPARK_DIST_CLASSPATH=$($HADOOP_HOME/bin/hadoop classpath)
