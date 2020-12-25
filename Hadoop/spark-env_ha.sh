d0837c5c8db419a0dc3d1f2134404e4b2e8a203d
Jun <33471088+151438611@users.noreply.github.com>
2020-12-25 11:50:56 +0800

    Update spark-env_ha.sh

--- Hadoop/spark-env_ha.sh
+++ Hadoop/spark-env_ha.sh
@@ -8,7 +8,6 @@ export SPARK_MASTER_WEBUI_PORT=8080
 
 export HADOOP_HOME=/usr/local/hadoop
 export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
-
 export SPARK_DIST_CLASSPATH=$($HADOOP_HOME/bin/hadoop classpath)
 
 export SPARK_HISTORY_OPTS="-Dspark.history.ui.port=18080 -Dspark.history.fs.logDirectory=hdfs://master:9000/spark_historyserver -Dspark.history.retainedApplications=30"
