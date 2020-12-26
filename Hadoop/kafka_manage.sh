dacfd50424c4a0b41141f6ac9d8ccc60a609564e
Jun <33471088+151438611@users.noreply.github.com>
2020-12-26 14:08:18 +0800

    Add files via upload

---
+++ Hadoop/kafka_manage.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# 用于在 master 主机上管理(启动/停止) kafka 集群服务
+# 提前: master和其他主机配置好SSH免密登陆, kafka 集群配置正确
+
+# 主机名或IP地址; 自行填写
+host1=slave1
+host2=slave2
+host3=slave3
+host4=
+host5=
+hosts="$host1 $host2 $host3 $host4 $host5"
+
+exec_PATH="/usr/local/kafka/bin"
+
+case $1 in
+	"start")
+		exec_command="$exec_PATH/kafka-server-start.sh -daemon $exec_PATH/../config/server.properties"
+	;;
+	"stop")
+		exec_command="$exec_PATH/kafka-server-stop.sh"
+	;;
+	*)
+		echo -e "Usage : \033[31m$0 [start | stop]\033[0m"
+		exit
+	;;
+esac
+
+for host in $hosts
+do
+	echo -e "\033[33m---------- $1 Host: $host ----------\033[0m"
+	ssh $host $exec_command
+done
+
--- Hadoop/zookeeper_manage.sh
+++ Hadoop/zookeeper_manage.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 # 用于在 master 主机上管理(启动/停止/查看) zookeeper 集群服务
-# 提前: master和其他主机配置好SSH免密登陆, zookeeper集群配置正确
+# 提前: master和其他主机配置好SSH免密登陆, zookeeper 集群配置正确
 
 # 主机名或IP地址; 自行填写
-zk1=slave1
-zk2=slave2
-zk3=slave3
-zk4=
-zk5=
-zookeepers="$zk1 $zk2 $zk3 $zk4 $zk5"
+host1=slave1
+host2=slave2
+host3=slave3
+host4=
+host5=
+hosts="$host1 $host2 $host3 $host4 $host5"
 
-zkPATH="/usr/local/zookeeper/bin"
+exec_PATH="/usr/local/zookeeper/bin"
 
 case $1 in
 	"start")
-		exec_command="$zkPATH/zkServer.sh start"
+		exec_command="$exec_PATH/zkServer.sh start"
 	;;
 	"stop")
-		exec_command="$zkPATH/zkServer.sh stop"
+		exec_command="$exec_PATH/zkServer.sh stop"
 	;;
 	"status")
-		exec_command="$zkPATH/zkServer.sh status"
+		exec_command="$exec_PATH/zkServer.sh status"
 	;;
 	*)
 		echo -e "Usage : \033[31m$0 [start | stop | status]\033[0m"
@@ -28,9 +28,9 @@ case $1 in
 	;;
 esac
 
-for zk_host in $zookeepers
+for host in $hosts
 do
-	echo -e "\033[33m---------- Host: $zk_host ----------\033[0m"
-	echo -e "\033[36m$(ssh $zk_host $exec_command 2> /dev/null)\033[0m\n"
+	echo -e "\033[33m---------- Host: $host ----------\033[0m"
+	echo -e "\033[36m$(ssh $host $exec_command 2> /dev/null)\033[0m\n"
 done
 
