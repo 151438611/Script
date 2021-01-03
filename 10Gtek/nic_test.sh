39a4794f7739fced70e0c829df77ec93b50e73e8
Jun <33471088+151438611@users.noreply.github.com>
2021-01-03 20:19:18 +0800

    Update nic_test.sh

--- 10Gtek/nic_test.sh
+++ 10Gtek/nic_test.sh
@@ -6,6 +6,15 @@
 
 #export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:$PATH
 clear
+blue_echo() {
+	echo -e "\033[36m$1\033[0m"
+}
+yellow_echo() {
+	echo -e "\033[33m$1\033[0m"
+}
+red_echo() {
+	echo -e "\033[31m$1\033[0m"
+}
 echo -e "\n测试环境要求："
 echo -e "\n1、测试电脑1(上)配置IP信息 eth1:192.168.6.101 eth2:192.168.7.101 eth3:192.168.8.101 eth4:192.168.9.101"
 echo "   测试电脑2(下)配置IP信息 eth1:192.168.6.201 eth2:192.168.7.201 eth3:192.168.8.201 eth4:192.168.9.201"
@@ -14,7 +23,7 @@ echo "   注意:尽量使用端口数量相同的网卡,端口数不同的网卡
 echo -e "   注意:有的网卡从上往下依次为eth1、eth2... ;有的网卡从下往上为eth1、eth2... \n"
 
 mark() { 
-  echo "==========================================================" 
+	echo "==========================================================" 
 }
 
 fun_mount_smb() {
@@ -29,7 +38,6 @@ fun_mount_smb() {
 fun_get_net_IP() {
 	# 传入参数: $1端口号/$2源ip尾号/$3目的ip尾号
 	[ $# -ne 3 ] && echo "请传入 端口号/源ip尾号/目的ip尾号 给 fun_get_net_IP 函数 !!!" && continue
-	
 	case $1 in
 		eth1) 
 			src_ip=192.168.6.$2
@@ -55,143 +63,143 @@ fun_get_net_IP() {
 }
 
 fun_save_log_1() {
-	# 需要传入$1网卡接口编号; 示例: eth1 eth2 .....
-	[ -z "$1" ] && echo "请传入网卡接口编号给 fun_save_log 函数 !!!" && continue
-	
-	log="/tmp/$1.txt"
-	echo -e "Test Start Time : $(date +"%F %T") \n" > $log 
+# 需要传入$1网卡接口编号; 示例: eth1 eth2 .....
+[ -z "$1" ] && echo "请传入网卡接口编号给 fun_save_log 函数 !!!" && continue
+
+log="/tmp/$1.txt"
+echo -e "Test Start Time : $(date +"%F %T") \n" > $log 
 }
 
 fun_get_net_hardware_2() {
-	# 不需要传入参数,仅需要 $log 为全局变量即可
-	net_hardware=$(lspci | grep -i "Ethernet controller")
-	net_hardware_num=$(echo "$net_hardware" | wc -l)
-	if [ $net_hardware_num -gt 1 -a $net_hardware_num -le 5 ]; then 
-		result="识别网卡成功"
-		echo -e "$result \n$net_hardware \n" | tee -a $log
-	elif [ $net_hardware_num -gt 5 ]; then 
-		result="识别网卡异常"
-		echo -e "$result \n$net_hardware \n" | tee -a $log
-		error_log=yes
-	else 
-		result="未识别插入的 PCI-E 网卡设备 !!!"
-		echo -e "$result , 请检查设备是否已插好,再重新启动测试 !!! \n$net_hardware"  | tee -a $log
-		exit
-	fi
-	mark
+# 不需要传入参数,仅需要 $log 为全局变量即可
+net_hardware=$(lspci | grep -i "Ethernet controller")
+net_hardware_num=$(echo "$net_hardware" | wc -l)
+if [ $net_hardware_num -gt 1 -a $net_hardware_num -le 5 ]; then 
+result="识别网卡成功"
+echo -e "$result \n$net_hardware \n" | tee -a $log
+elif [ $net_hardware_num -gt 5 ]; then 
+result="识别网卡异常"
+echo -e "$result \n$net_hardware \n" | tee -a $log
+error_log=yes
+else 
+result="未识别插入的 PCI-E 网卡设备 !!!"
+echo -e "$result , 请检查设备是否已插好,再重新启动测试 !!! \n$net_hardware"  | tee -a $log
+exit
+fi
+mark
 }
 
 fun_get_net_driver_3() {
-	# 需要传入$1网卡接口编号; 示例: eth1 eth2 .....
-	[ -z "$1" ] && echo "请传入网卡接口编号给 fun_get_net_driver 函数 !!!" && continue
-	
-	ethtool -i $1 &> /dev/null && driver_result="读取网卡驱动版本信息成功" || { driver_result="读取网卡驱动版本信息失败 !!!"; error_log=yes; }
-	echo -e "\n$driver_result" | tee -a $log 
-	ethtool -i $1 | tee -a $log
-	mark
+# 需要传入$1网卡接口编号; 示例: eth1 eth2 .....
+[ -z "$1" ] && echo "请传入网卡接口编号给 fun_get_net_driver 函数 !!!" && continue
+
+ethtool -i $1 &> /dev/null && driver_result="读取网卡驱动版本信息成功" || { driver_result="读取网卡驱动版本信息失败 !!!"; error_log=yes; }
+echo -e "\n$driver_result" | tee -a $log 
+ethtool -i $1 | tee -a $log
+mark
 }
 
 fun_get_net_eeprom_4() {
-	# 需要传入$1网卡接口编号; 示例: eth1 eth2 .....
-	[ -z "$1" ] && echo "请传入网卡接口编号给 fun_get_net_eeprom 函数 !!!" && continue
-	
-	ethtool -m $1 && eeprom_result="读取EEPROM信息成功" || eeprom_result="读取EEPROM信息失败 !!! <说明:若网卡是RJ45接口则无EEPROM>"
-	echo -e "\n$eeprom_result" | tee -a $log 
-	ethtool -m $1 &>> $log
-	mark
+# 需要传入$1网卡接口编号; 示例: eth1 eth2 .....
+[ -z "$1" ] && echo "请传入网卡接口编号给 fun_get_net_eeprom 函数 !!!" && continue
+
+ethtool -m $1 && eeprom_result="读取EEPROM信息成功" || eeprom_result="读取EEPROM信息失败 !!! <说明:若网卡是RJ45接口则无EEPROM>"
+echo -e "\n$eeprom_result" | tee -a $log 
+ethtool -m $1 &>> $log
+mark
 }
 
 fun_get_net_link_5() {
-	# 需要传入$1网卡接口编号; 示例: eth1 eth2 .....
-	[ -z "$1" ] && echo "请传入网卡接口编号给 fun_get_net_link 函数 !!!" && continue
-	
-	link_info=$(ethtool $1 2> /dev/null)
-	link_status=$(echo "$link_info" | awk '/Link detected:/{print $3}')
-	link_speed=$(echo "$link_info" |awk '/Speed:/{print int($2)}')
-	
-	if [ "$link_status" = yes -a -n "link_speed" ]; then
-		link_result="链路已连通,速率为 $link_speed Mb/s"
-	else 
-		link_result="链路连通失败 !!! 无法继续下一步 ping包 和 iperf3性能测试 ..." 
-		error_log=yes
-	fi
-	echo -e "\n$link_result" | tee -a $log
-	echo "$link_info" | tee -a $log
-	mark
+# 需要传入$1网卡接口编号; 示例: eth1 eth2 .....
+[ -z "$1" ] && echo "请传入网卡接口编号给 fun_get_net_link 函数 !!!" && continue
+
+link_info=$(ethtool $1 2> /dev/null)
+link_status=$(echo "$link_info" | awk '/Link detected:/{print $3}')
+link_speed=$(echo "$link_info" |awk '/Speed:/{print int($2)}')
+
+if [ "$link_status" = yes -a -n "link_speed" ]; then
+link_result="链路已连通,速率为 $link_speed Mb/s"
+else 
+link_result="链路连通失败 !!! 无法继续下一步 ping包 和 iperf3性能测试 ..." 
+error_log=yes
+fi
+echo -e "\n$link_result" | tee -a $log
+echo "$link_info" | tee -a $log
+mark
 }
 
 fun_ping_test_6() {
-	# 依赖 fun_get_net_IP 函数, 获取变量 $dest_ip 
-	# 需要传入: $1目的IP / $2测试次数
-	[ $# -ne 2 ] && echo "请传入 目的IP/Ping包次数 给 fun_ping_test 函数 !!!" && continue
-	
-	if [ "$link_status" = yes ]; then
-		echo -e "\n正在进行 $ping_count 次的 Ping 包测试 ......"
-		pinglog=/tmp/ping$1.log
-		ping -c 2 -w 3 $1 &> $pinglog &&  ping -c $2 -i 0.1 $1 | tee $pinglog
-		[ "$(awk '/ 0% packet loss/ {print $0}' $pinglog)" ] && ping_result="Ping包成功,无丢包" || { ping_result="Ping包失败,或有丢包 !!!"; error_log=yes; }
-		echo -e "\n$ping_result" | tee -a $log
-		echo -e "$(head $pinglog) \n......\n$(tail $pinglog) " >> $log
-	fi
-	mark
+# 依赖 fun_get_net_IP 函数, 获取变量 $dest_ip 
+# 需要传入: $1目的IP / $2测试次数
+[ $# -ne 2 ] && echo "请传入 目的IP/Ping包次数 给 fun_ping_test 函数 !!!" && continue
+
+if [ "$link_status" = yes ]; then
+echo -e "\n正在进行 $ping_count 次的 Ping 包测试 ......"
+pinglog=/tmp/ping$1.log
+ping -c 2 -w 3 $1 &> $pinglog &&  ping -c $2 -i 0.1 $1 | tee $pinglog
+[ "$(awk '/ 0% packet loss/ {print $0}' $pinglog)" ] && ping_result="Ping包成功,无丢包" || { ping_result="Ping包失败,或有丢包 !!!"; error_log=yes; }
+echo -e "\n$ping_result" | tee -a $log
+echo -e "$(head $pinglog) \n......\n$(tail $pinglog) " >> $log
+fi
+mark
 }
 
 fun_iperf_test_7() {
-	# 依赖 fun_get_net_IP 函数, 获取变量 $src_ip / $dest_ip / $iperf_port
-	# 需要传入: $1源IP / $2目的IP / $3端口号 / $4测试时长
-	[ $# -ne 4 ] && echo "请传入 源IP/目的IP/端口号/测试时长 给 fun_iperf_test 函数 !!!" && continue
-	
-	if [ "$link_status" = yes ]; then
-		echo -e "\n正在进行 iperf3 性能测试,请稍等 $4 秒 ......"
-		iperflog=/tmp/iperf$2.log
-		iperf3 -V -B $1 -c $2 -p $3 -t $4 > $iperflog
+# 依赖 fun_get_net_IP 函数, 获取变量 $src_ip / $dest_ip / $iperf_port
+# 需要传入: $1源IP / $2目的IP / $3端口号 / $4测试时长
+[ $# -ne 4 ] && echo "请传入 源IP/目的IP/端口号/测试时长 给 fun_iperf_test 函数 !!!" && continue
 
-		# 获取网卡测试速率，并将单位 Gbits/sec 转换成 Mbits/sec
-		grep -q "\[SUM\]" $iperflog && {
-				iperf_speed=$(awk '/SUM/ && /sender/ && /bits\/sec/ {if($7 == "Gbits/sec"){print int($6 * 1024)} else {print $6}}' $iperflog)
-			} || {
-				iperf_speed=$(awk '/sender/ && /bits\/sec/ {if($8 == "Gbits/sec"){print int($7 * 1024)} else {print $7}}' $iperflog)
-			}
-		# 判断测试速率是否达标网卡协商速率的90%
-		iperf_speed_result=
-		case $link_speed in
-			10) [ "$iperf_speed" -ge 9 -a "$iperf_speed" -lt 10 ] && iperf_speed_result=ok ;;
-			100) [ "$iperf_speed" -ge 90 -a "$iperf_speed" -lt 100 ] && iperf_speed_result=ok ;;
-			1000) [ "$iperf_speed" -ge 900 -a "$iperf_speed" -lt 1000 ] && iperf_speed_result=ok ;;
-			2500) [ "$iperf_speed" -ge 2250 -a "$iperf_speed" -lt 2500 ] && iperf_speed_result=ok ;;
-			5000) [ "$iperf_speed" -ge 4500 -a "$iperf_speed" -lt 5000 ] && iperf_speed_result=ok ;;
-			10000) [ "$iperf_speed" -ge 9000 -a "$iperf_speed" -lt 10000 ] && iperf_speed_result=ok ;;
-			25000) [ "$iperf_speed" -ge 22500 -a "$iperf_speed" -lt 25000 ] && iperf_speed_result=ok ;;
-			40000) [ "$iperf_speed" -ge 36000 -a "$iperf_speed" -lt 40000 ] && iperf_speed_result=ok ;;
-			100000) [ "$iperf_speed" -ge 90000 -a "$iperf_speed" -lt 100000 ] && iperf_speed_result=ok ;;
-		esac
-		[ -n "$(grep "iperf Done" $iperflog)" -a "$iperf_speed_result" = ok ] && iperf_result="性能测试<$iperf_speed Mbits/sec>完成" || { iperf_result="性能测试<$iperf_speed Mbits/sec>失败 !!!"; error_log=yes; }
-		echo -e "\n$iperf_result" | tee -a $log
-		echo -e "$(head -n 20 $iperflog) \n......\n$(tail -n 20 $iperflog)" >> $log 
-	fi
-	mark
+if [ "$link_status" = yes ]; then
+echo -e "\n正在进行 iperf3 性能测试,请稍等 $4 秒 ......"
+iperflog=/tmp/iperf$2.log
+iperf3 -V -B $1 -c $2 -p $3 -t $4 > $iperflog
+
+# 获取网卡测试速率，并将单位 Gbits/sec 转换成 Mbits/sec
+grep -q "\[SUM\]" $iperflog && {
+iperf_speed=$(awk '/SUM/ && /sender/ && /bits\/sec/ {if($7 == "Gbits/sec"){print int($6 * 1024)} else {print $6}}' $iperflog)
+} || {
+iperf_speed=$(awk '/sender/ && /bits\/sec/ {if($8 == "Gbits/sec"){print int($7 * 1024)} else {print $7}}' $iperflog)
+}
+# 判断测试速率是否达标网卡协商速率的90%
+iperf_speed_result=
+case $link_speed in
+10) [ "$iperf_speed" -ge 9 -a "$iperf_speed" -lt 10 ] && iperf_speed_result=ok ;;
+100) [ "$iperf_speed" -ge 90 -a "$iperf_speed" -lt 100 ] && iperf_speed_result=ok ;;
+1000) [ "$iperf_speed" -ge 900 -a "$iperf_speed" -lt 1000 ] && iperf_speed_result=ok ;;
+2500) [ "$iperf_speed" -ge 2250 -a "$iperf_speed" -lt 2500 ] && iperf_speed_result=ok ;;
+5000) [ "$iperf_speed" -ge 4500 -a "$iperf_speed" -lt 5000 ] && iperf_speed_result=ok ;;
+10000) [ "$iperf_speed" -ge 9000 -a "$iperf_speed" -lt 10000 ] && iperf_speed_result=ok ;;
+25000) [ "$iperf_speed" -ge 22500 -a "$iperf_speed" -lt 25000 ] && iperf_speed_result=ok ;;
+40000) [ "$iperf_speed" -ge 36000 -a "$iperf_speed" -lt 40000 ] && iperf_speed_result=ok ;;
+100000) [ "$iperf_speed" -ge 90000 -a "$iperf_speed" -lt 100000 ] && iperf_speed_result=ok ;;
+esac
+[ -n "$(grep "iperf Done" $iperflog)" -a "$iperf_speed_result" = ok ] && iperf_result="性能测试<$iperf_speed Mbits/sec>完成" || { iperf_result="性能测试<$iperf_speed Mbits/sec>失败 !!!"; error_log=yes; }
+echo -e "\n$iperf_result" | tee -a $log
+echo -e "$(head -n 20 $iperflog) \n......\n$(tail -n 20 $iperflog)" >> $log 
+fi
+mark
 }
 
 fun_copy_result_8() {
-	# 需要传入: $1目的文件夹
-	#[ "$1" ] || { echo "请传入 目的文件夹 给 fun_copy_result 函数 !!!" && continue; }
-	unix2dos -o $log &> /dev/null
-	echo -e "\n测试完成,测试数据保存在 $log ,下次测试会覆盖掉,请及时拷出 !!! \n"
+# 需要传入: $1目的文件夹
+#[ "$1" ] || { echo "请传入 目的文件夹 给 fun_copy_result 函数 !!!" && continue; }
+unix2dos -o $log &> /dev/null
+echo -e "\n测试完成,测试数据保存在 $log ,下次测试会覆盖掉,请及时拷出 !!! \n"
 
-	smb_user=GCB01 
-	smb_password="*WGQGf"
-	smb_src=//192.168.10.250/gc-fae/faeTest/nictest/nic_test_report
-	smb_dest=/media/nictest
-	fun_mount_smb $smb_user $smb_password $smb_src $smb_dest
+smb_user=GCB01 
+smb_password="*WGQGf"
+smb_src=//192.168.10.250/gc-fae/faeTest/nictest/nic_test_report
+smb_dest=/media/nictest
+fun_mount_smb $smb_user $smb_password $smb_src $smb_dest
 
-	if [ "$(mount | grep $smb_src)" ]; then
-		[ -d $smb_dest/$1 ] || mkdir -p $smb_dest/$1
-		[ "$1" ] && { [ "$error_log" ] && cp -f $log $smb_dest/$1/error_${log##*/} || cp -f $log $smb_dest/$1 ; } || \
-			{ [ "$error_log" ] && cp -f $log $smb_dest/error_${log##*/} || cp -f $log $smb_dest/ ; }
-		echo "测试数据 $log 已复制到 $smb_src/$1 ,下次测试会覆盖掉,请及时拷出 !!! "
-	fi
-	# umount $src
-	mark
+if [ "$(mount | grep $smb_src)" ]; then
+[ -d $smb_dest/$1 ] || mkdir -p $smb_dest/$1
+[ "$1" ] && { [ "$error_log" ] && cp -f $log $smb_dest/$1/error_${log##*/} || cp -f $log $smb_dest/$1 ; } || \
+{ [ "$error_log" ] && cp -f $log $smb_dest/error_${log##*/} || cp -f $log $smb_dest/ ; }
+echo "测试数据 $log 已复制到 $smb_src/$1 ,下次测试会覆盖掉,请及时拷出 !!! "
+fi
+# umount $src
+mark
 }
 
 # =============下面开始功能代码,上面是函数定义===========================
@@ -200,11 +208,11 @@ net_interface_num=$(echo "$net_interface" | wc -l)
 [ $net_interface_num -lt 1 -o $net_interface_num -gt 4 ] && echo "网卡接口号显示异常:< $net_interface_num > !!! " && exit 1 
 
 if [ "$(ip address | grep inet | grep -E "192.168.6.101|192.168.7.101|192.168.8.101|192.168.9.101")" ]; then
-	src_ip_end=101
-	dest_ip_end=201
+src_ip_end=101
+dest_ip_end=201
 elif [ "$(ip address | grep inet | grep -E "192.168.6.201|192.168.7.201|192.168.8.201|192.168.9.201")" ]; then
-	src_ip_end=201
-	dest_ip_end=101
+src_ip_end=201
+dest_ip_end=101
 fi
 
 # 清除iperf进程
@@ -212,78 +220,78 @@ killall -q iperf3
 
 read -p "此端是服务端<S>还是客户端<C>? 默认客户端<C>,请输入 <S/C> : " type
 case $type in
-	S|s) 
-		net_start=6
-		port_start=5201
-		for net_num in $(seq $net_interface_num)
-		do
-			iperf3 -s -B 192.168.${net_start}.$src_ip_end -p $port_start &
-			let net_start+=1
-			let port_start+=1
-		done
-	;;
-	*)
-		#read -p "确认测试环境是否已配置正确,默认yes,请输入 <yes/no> : " confirm
-		#[ "${confirm:=yes}" != yes ] && echo -e "\n请先配置好测试环境，再重新测试!\n" && exit
+S|s) 
+net_start=6
+port_start=5201
+for net_num in $(seq $net_interface_num)
+do
+iperf3 -s -B 192.168.${net_start}.$src_ip_end -p $port_start &
+let net_start+=1
+let port_start+=1
+done
+;;
+*)
+#read -p "确认测试环境是否已配置正确,默认yes,请输入 <yes/no> : " confirm
+#[ "${confirm:=yes}" != yes ] && echo -e "\n请先配置好测试环境，再重新测试!\n" && exit
 
-		echo -e "\n所有网卡端口列表(state UP表示端口已链接, state DOWN表示端口未链接) : \n$net_interface \n"
-		read -p "请输入测试的网卡端口号,默认 eth1 ,请输入 < 1/2/3/4/all > : " port_id_tmp
-		echo ""
+echo -e "\n所有网卡端口列表(state UP表示端口已链接, state DOWN表示端口未链接) : \n$net_interface \n"
+read -p "请输入测试的网卡端口号,默认 eth1 ,请输入 < 1/2/3/4/all > : " port_id_tmp
+echo ""
 
-		read -p "请输入 Ping 包次数,默认 2000 次,请输入 < 2000-10000 > : " ping_count
-		ping_count=${ping_count:=2000}
-		[ -n "$(echo $ping_count | tr -d [0-9])" ] && ping_count=2000
-		echo ""
+read -p "请输入 Ping 包次数,默认 2000 次,请输入 < 2000-10000 > : " ping_count
+ping_count=${ping_count:=2000}
+[ -n "$(echo $ping_count | tr -d [0-9])" ] && ping_count=2000
+echo ""
 
-		read -p "请输入 iperf3 性能测试时长,默认 60 秒,请输入自定义时间,单位为秒 : " iperf_time
-		iperf_time=${iperf_time:=60}
-		[ -n "$(echo $iperf_time | tr -d [0-9])" ] && iperf_time=60
-		echo ""
-		
-		read -p "请输入测试数据的存放目录,默认放在根目录下 : " save_dir
-		save_dir=$(echo $save_dir | sed -r 's/[[:space:]]//g')
+read -p "请输入 iperf3 性能测试时长,默认 60 秒,请输入自定义时间,单位为秒 : " iperf_time
+iperf_time=${iperf_time:=60}
+[ -n "$(echo $iperf_time | tr -d [0-9])" ] && iperf_time=60
+echo ""
 
-		echo -e "\n开始自动进行测试: \n"
-		# 初始化错误日志
-		error_log=
+read -p "请输入测试数据的存放目录,默认放在根目录下 : " save_dir
+save_dir=$(echo $save_dir | sed -r 's/[[:space:]]//g')
 
-		# =========================== 开始测试功能 ===========================
-		case ${port_id_tmp:=1} in 
-			1|2|3|4) 
-				port_id=eth$port_id_tmp 
-				fun_get_net_IP $port_id $src_ip_end $dest_ip_end
-				fun_save_log_1 $port_id
-				fun_get_net_hardware_2
-				fun_get_net_driver_3 $port_id
-				fun_get_net_eeprom_4 $port_id
-				fun_get_net_link_5 $port_id
-				fun_ping_test_6 $dest_ip $ping_count
-				fun_iperf_test_7 $src_ip $dest_ip $iperf_port $iperf_time
-				fun_copy_result_8 $save_dir
-			;; 
-			a|all)
-				for port_id_tmp1 in $(seq $net_interface_num)
-				do
-					# 使用{ } & 将任务放入后台,同时测试; 取消{ }则测试eth1再依次测试eth2 ... 
-					{ 
-						port_id=eth$port_id_tmp1
-						fun_get_net_IP $port_id $src_ip_end $dest_ip_end
-						fun_save_log_1 $port_id
-						fun_get_net_hardware_2
-						fun_get_net_driver_3 $port_id
-						fun_get_net_eeprom_4 $port_id
-						fun_get_net_link_5 $port_id
-						fun_ping_test_6 $dest_ip $ping_count
-						fun_iperf_test_7 $src_ip $dest_ip $iperf_port $iperf_time
-						fun_copy_result_8 $save_dir
-					} &
-				done
-			;;
-			*) 
-				echo -e "\n请输入有效的网卡编号，再重新测试!\n" && exit 1 
-			;;
-		esac
+echo -e "\n开始自动进行测试: \n"
+# 初始化错误日志
+error_log=
+
+# =========================== 开始测试功能 ===========================
+case ${port_id_tmp:=1} in 
+1|2|3|4) 
+port_id=eth$port_id_tmp 
+fun_get_net_IP $port_id $src_ip_end $dest_ip_end
+fun_save_log_1 $port_id
+fun_get_net_hardware_2
+fun_get_net_driver_3 $port_id
+fun_get_net_eeprom_4 $port_id
+fun_get_net_link_5 $port_id
+fun_ping_test_6 $dest_ip $ping_count
+fun_iperf_test_7 $src_ip $dest_ip $iperf_port $iperf_time
+fun_copy_result_8 $save_dir
+;; 
+a|all)
+for port_id_tmp1 in $(seq $net_interface_num)
+do
+# 使用{ } & 将任务放入后台,同时测试; 取消{ }则测试eth1再依次测试eth2 ... 
+{ 
+port_id=eth$port_id_tmp1
+fun_get_net_IP $port_id $src_ip_end $dest_ip_end
+fun_save_log_1 $port_id
+fun_get_net_hardware_2
+fun_get_net_driver_3 $port_id
+fun_get_net_eeprom_4 $port_id
+fun_get_net_link_5 $port_id
+fun_ping_test_6 $dest_ip $ping_count
+fun_iperf_test_7 $src_ip $dest_ip $iperf_port $iperf_time
+fun_copy_result_8 $save_dir
+} &
+done
+;;
+*) 
+echo -e "\n请输入有效的网卡编号，再重新测试!\n" && exit 1 
+;;
+esac
 
 
-	;;
+;;
 esac
