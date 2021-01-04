2636d580483f3e56900a4995658935cf9b3ba45a
Jun <33471088+151438611@users.noreply.github.com>
2021-01-04 11:29:45 +0800

    Update nic_test.sh

--- 10Gtek/nic_test.sh
+++ 10Gtek/nic_test.sh
@@ -274,6 +274,7 @@ fun_copy_result_8() {
 	#[ "$1" ] || { echo "请传入 目的文件夹 给 fun_copy_result 函数" && continue; }
 	unix2dos -o $log &> /dev/null
 	yellow_echo "\n测试完成,测试数据保存在 $log ,下次测试会覆盖掉,请及时拷出. \n"
+    # 解决测试网卡作为默认网关，无法正常上网的问题
 	[ "$(ip route | head -n 1 | grep 192.168.200.1)" ] && {
 		for gw in $(ip route | grep default | grep -v 192.168.200.1 | awk '{print $3}')
 		do
