a9527494cdee833b5f30b05967789f653bf5eb02
Jun <33471088+151438611@users.noreply.github.com>
2021-01-07 13:35:24 +0800

    Update 6_scp_armbianN1_to_padavan.sh

--- 6_scp_armbianN1_to_padavan.sh
+++ 6_scp_armbianN1_to_padavan.sh
@@ -14,44 +14,41 @@ src2=/media/sda1/data/script/Script-master.zip
 src3=
 src4=
 src5=
-src6=
-src7=
-src8=
-source="$src0 $src1 $src2 $src3 $src4 $src5 $src6 $src7 $src8"
+source="$src0 $src1 $src2 $src3 $src4 $src5"
 
 # ------- scp to jhk2p_75_usb ----------
 if [ "$router" = jhk2p ]; then
-  dest_ip=admin@frp.xxy1.ltd
-  dest_dir=/media/AiDisk_a2/data
-  dest_port=35175
-  frp_dir=/media/AiDisk_a2/frp/
+    dest_ip=admin@frp.xxy1.ltd
+    dest_dir=/media/AiDisk_a2/data
+    dest_port=35175
+    frp_dir=/media/AiDisk_a2/frp/
 fi
 # ------- scp to youku-L1_TF ----------
 if [ "$router" = youku ]; then
-  dest_ip=admin@frp.xxy1.ltd
-  dest_dir=/media/AiCard_01/data
-  dest_port=35111
-  frp_dir=/media/AiCard_01/frp/
+    dest_ip=admin@frp.xxy1.ltd
+    dest_dir=/media/AiCard_01/data
+    dest_port=35111
+    frp_dir=/media/AiCard_01/frp/
 fi
 # ------- scp to szk2p_usb ----------
 if [ "$router" = szk2p ]; then
-  dest_ip=admin@frp.xxy1.ltd
-  dest_dir=/media/AiDisk_a1/data
-  dest_port=35320
-  frp_dir=/media/AiDisk_a1/frp/
+    dest_ip=admin@frp.xxy1.ltd
+    dest_dir=/media/AiDisk_a1/data
+    dest_port=35320
+    frp_dir=/media/AiDisk_a1/frp/
 fi
 
 dest=${dest_ip}:${dest_dir}
 scp_fun() {
-# $1:source dir/file  ,  $2:dest dir/file
-  scp -r -o "StrictHostKeyChecking no" -P ${dest_port:-22} $1 $2 && \
-  echo "$(date +"%F %T") scp to $router success $1" >> $scplog || echo "$(date +"%F %T") scp to $router fail--- $1" >> $scplog
+    # $1:source dir/file  ,  $2:dest dir/file
+    scp -r -o "StrictHostKeyChecking no" -P ${dest_port:-22} $1 $2 && \
+    echo "$(date +"%F %T") scp to $router success $1" >> $scplog || echo "$(date +"%F %T") scp to $router fail--- $1" >> $scplog
 }
 
 # === start scp ==============
 for src in $source
 do
-  scp_fun $src $dest
+    scp_fun $src $dest
 done
 
 # add temp frp file
