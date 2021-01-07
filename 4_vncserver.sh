1fc2b357e0d9d4f679c7f2f0b233a9f1be3946da
Jun <33471088+151438611@users.noreply.github.com>
2021-01-07 13:36:24 +0800

    Update 4_vncserver.sh

--- 4_vncserver.sh
+++ 4_vncserver.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 case "$1" in
-start)	
-	[ "$(pidof Xtigervnc)" ] || \
-	/usr/bin/vncserver -localhost no -depth 24 -geometry 1920x1080 -SecurityTypes VncAuth
-;;
-stop)	
-	/usr/bin/vncserver -kill :* -clean
-;;
-status)
-	/usr/bin/vncserver -list
-;;
-restart) 
-	$0 stop
-	sleep 2
-	$0 start
-;;
-*)	
-	echo "Usage: bash vncserver.sh {start|stop|status|restart}"
-	exit 2
-;;
+	start)	
+		[ "$(pidof Xtigervnc)" ] || \
+		/usr/bin/vncserver -localhost no -depth 24 -geometry 1920x1080 -SecurityTypes VncAuth
+	;;
+	stop)	
+		/usr/bin/vncserver -kill :* -clean
+	;;
+	status)
+		/usr/bin/vncserver -list
+	;;
+	restart) 
+		$0 stop
+		sleep 2
+		$0 start
+	;;
+	*)	
+		echo "Usage: bash vncserver.sh {start|stop|status|restart}"
+		exit 2
+	;;
 esac
