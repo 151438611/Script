d6d80f3121d6ea93a577d1c71cafba8237964730
Jun <33471088+151438611@users.noreply.github.com>
2021-03-18 17:00:50 +0800

    Update spark-env_ha.sh

--- Hadoop/spark-env_ha.sh
+++ Hadoop/spark-env_ha.sh
@@ -10,6 +10,8 @@ export HADOOP_HOME=/usr/local/hadoop
 export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
 export SPARK_DIST_CLASSPATH=$($HADOOP_HOME/bin/hadoop classpath)
 
-export SPARK_HISTORY_OPTS="-Dspark.history.ui.port=18080 -Dspark.history.fs.logDirectory=hdfs://master:9000/spark/historyserver -Dspark.history.retainedApplications=30"
+# 若 spark-defaults.conf 中配置 spark.eventLog.enabled	true 则开启此选项
+# export SPARK_HISTORY_OPTS="-Dspark.history.ui.port=18080 -Dspark.history.fs.logDirectory=hdfs://master:9000/spark/historyserver -Dspark.history.retainedApplications=30"
 
 export SPARK_DAEMON_JAVA_OPTS="-Dspark.deploy.recoveryMode=ZOOKEEPER -Dspark.deploy.zookeeper.url=slave1:2181,slave2:2181,slave3:2181 -Dspark.deploy.zookeeper.dir=/spark"
+
