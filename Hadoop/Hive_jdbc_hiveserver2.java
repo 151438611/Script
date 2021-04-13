23a91e2f7a827bd109dd60f0fb95088f6ff04a16
Jun <33471088+151438611@users.noreply.github.com>
2021-04-13 22:12:15 +0800

    Update and rename WordCount_1.java to Hive_jdbc_hiveserver2.java

---
+++ Hadoop/Hive_jdbc_hiveserver2.java
@@ -0,0 +1,21 @@
+// Hadoop 2.10.1、Hive 2.3.8 测试 OK
+
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.DriverManager;
+
+public class testhive {
+ private static String drivername = "org.apache.hive.jdbc.HiveDriver";
+ public static void main(String[] args) throws 	SQLException, ClassNotFoundException{
+	 //register driver and create driver instance
+	 Class.forName(drivername);
+	 //get connection 
+	 Connection con = DriverManager.getConnection("jdbc:hive2://192.168.200.168:10000/default","","");
+	 Statement stmt = con.createStatement();
+	 stmt.execute("create database testHivedb123");
+	 System.out.println("Database userdb createed successfully");
+	 con.close(); 
+ }
+}
