5485bf8f54ef930d17d420e2913890283f9c0a51
Jun <33471088+151438611@users.noreply.github.com>
2021-04-14 11:23:04 +0800

    Update hive_jdbc_hiveserver2.java

--- Hadoop/hive_jdbc_hiveserver2.java
+++ Hadoop/hive_jdbc_hiveserver2.java
@@ -7,15 +7,16 @@ import java.sql.Statement;
 import java.sql.DriverManager;
 
 public class hive_jdbc_hiveserver2 {
- private static String hiveDriverName = "org.apache.hive.jdbc.HiveDriver";
- public static void main(String[] args) throws 	SQLException, ClassNotFoundException{
-	 //register driver and create driver instance
-	 Class.forName(hiveDriverName);
-	 //get connection 
-	 Connection con = DriverManager.getConnection("jdbc:hive2://192.168.200.168:10000/default","","");
-	 Statement stmt = con.createStatement();
-	 stmt.execute("create database testHivedb123");
-	 System.out.println("Database userdb createed successfully");
-	 con.close(); 
- }
+	private static String hiveDriverName = "org.apache.hive.jdbc.HiveDriver";
+	public static void main(String[] args) throws 	SQLException, ClassNotFoundException{
+		//register driver and create driver instance
+		Class.forName(hiveDriverName);
+		//get connection 
+		Connection con = DriverManager.getConnection("jdbc:hive2://192.168.200.168:10000/default","","");
+		Statement stmt = con.createStatement();
+		stmt.execute("create database testHivedb123");
+		System.out.println("Database userdb createed successfully");
+		con.close(); 
+	}
 }
+
