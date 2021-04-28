a3bb747634127b23ba0fbc5883a81f176197cb1f
Jun <33471088+151438611@users.noreply.github.com>
2021-04-28 14:36:45 +0800

    Update 1_hadoop_install.sh

--- Hadoop/1_hadoop_install.sh
+++ Hadoop/1_hadoop_install.sh
@@ -6,7 +6,7 @@
 # 20210318 更新：添加 Zookeeper 伪集群自动安装配置：zk1、zoo1.cfg / zk2、zoo2.cfg / zk3、zoo3.cfg
 # 20210409 更新：修改 Hadoop 的 hdfs-site.xml 配置中的 name/data 存储路径格式由 /xx/xx 改为 file:///xx/xx ; 以兼容 Hadoop 2.8 及以下版本,否则 namenode 日志中会有相关WARN信息
 # 20210413 更新：添加 Cassandra 安装配置功能
-# 20210413 更新：添加 Hadoop HBase 的HA高可用配置
+# 20210428 更新：添加 Hadoop HBase 的HA高可用配置
 # 测试OK : Hadoop 2.7.7/2.8.5/2.9.2/2.10.1/3.1.3/3.2.2/3.3.0; Spark 2.4.7/3.1.1
 
 # 以下变量可自行修改; 注意：1、写绝对路径； 2、install_dir安装目录需有读写权限；
