63b7ef60c3da81dcc84cd944444408efa2bc53f2
Jun <33471088+151438611@users.noreply.github.com>
2021-04-28 09:30:02 +0800

    Update hive-env.sh

--- Hadoop/hive-env.sh
+++ Hadoop/hive-env.sh
@@ -6,6 +6,6 @@ export HADOOP_HOME=/usr/local/hadoop
 export HIVE_HOME=/usr/local/hive
 export HIVE_CONF_DIR=$HIVE_HOME/conf
 
-# 用于hive和hbase整合
+# 用于hive和hbase整合配置
 export HBASE_HOME=/usr/local/hbase
 export HBASE_CONF_DIR=$HBASE_HOME/conf
