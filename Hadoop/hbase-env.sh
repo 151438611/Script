144b7cebb8d4153345123b223ec8d61888ac84a4
Jun <33471088+151438611@users.noreply.github.com>
2021-04-28 09:32:03 +0800

    Update hbase-env.sh

--- Hadoop/hbase-env.sh
+++ Hadoop/hbase-env.sh
@@ -1,8 +1,8 @@
-# HBase
+# HBase config
 
 export JAVA_HOME=/usr/local/java
-
 #export HBASE_CLASSPATH=$HBASE_HOME/conf
 
-#export HBASE_MANAGES_ZK=false
+# Config HBase HA
+export HBASE_MANAGES_ZK=false
 
