6a91d0355b35c93df023636ff387faafb1b403f5
Jun <33471088+151438611@users.noreply.github.com>
2021-05-08 08:58:03 +0800

    Add files via upload

--- 10Gtek/10gtek_product_password/index.php
+++ 10Gtek/10gtek_product_password/index.php
@@ -12,22 +12,83 @@
 	
 </head>
 <body>
+    
+<table border="1" align=right>
+    
+<tr >
+    <th colspan="2" bgcolor="#F5F5DC">10Gtek自产DAC、AOC、QSA改码密码汇总</th>
+    
+</tr>
+<tr>
+    <td align=right><font size="2">10G-SFP+ MCU DAC</font></td>
+    <td><font size="2">首选FF FF FF FF；备选38 34 37 32</font></td>
+</tr>
+<tr>
+    <td align=right><font size="2">10G-XFP MCU DAC</font></td>
+    <td><font size="2">38 34 37 32</font></td>
+</tr>
+<tr>
+    <td align=right><font size="2">25G-ZSP MCU DAC</font></td>
+    <td><font size="2">首选FF FF FF FF；<br>备选22 44 55 88 / 07 04 04 01</font></td>
+</tr>
+<tr>
+    <td align=right><font size="2">40G-Q10、100G-ZQP MCU DAC</font></td>
+    <td><font size="1">首选07 04 04 01；<br>备选LMM:22 44 55 88, P00:A0 03 07 31, <br>P02:00 00 10 11, P03:A0 03 07 31</font></td>
+</tr>
+<tr>
+    <td align=right><font size="2">100G-ZQP 1063定制 MCU DAC</font></td>
+    <td><font size="2">A0 03 07 31</font></td>
+</tr>
+<tr>
+    <td align=right><font size="2">100G-ZQP 3676定制 MCU DAC</font></td>
+    <td><font size="2">12 34 56 78</font></td>
+</tr>
+<tr>
+    <td align=right><font size="2">200G-Q56/QDD、400G-Q400 MCU DAC</font></td>
+    <td><font size="2">07 04 04 01</font></td>
+</tr>
+<tr>
+    <td align=right><font size="2">WADQS-10-B、WADQS-28-B MCU QSA</font></td>
+    <td><font size="2">07 04 04 01</font></td>
+</tr>
+<tr>
+    <td align=right><font size="2">EEPROM-DAC、8644-DAC</font></td>
+    <td><font size="2">无密码</font></td>
+</tr>
+<tr>
+    <td align=right><font size="2">10G-AOC</font></td>
+    <td><font size="2">53 46 50 2B</font></td>
+</tr>
+<tr>
+    <td align=right><font size="2">25G-AOC</font></td>
+    <td><font size="2">00 00 10 11</font></td>
+</tr>
+<tr>
+    <td align=right><font size="2">40G、56G、100G-AOC</font></td>
+    <td><font size="2">51 53 46 50</font></td>
+</tr>
+</table>
 	<a href="https://wx2.qq.com/" target="_self"><img border="0" src="./weixin.jpg" /></a>
 	<a href="http://192.168.200.21/phpmyadmin" target="_blank"><img border="0" src="./mariadb.jpg" /></a>
 	<a href="https://192.168.200.21" target="_self"><img border="0" src="./kodexplorer.jpg" /></a>
 	<a href="https://github.com/151438611/Script/" target="_blank"><img border="0" src="./github.jpg" /></a>
-	<a href="http://192.168.200.21/os/" target="_blank"><img border="0" src="./os/os.jpg" /></a>
 	<a href="https://mirrors.aliyun.com/apache/hadoop/common/" target="_blank"><img border="0" src="./hadoop.jpg" /></a>
 	<a href="https://mirrors.aliyun.com/apache/hbase/" target="_blank"><img border="0" src="./hbase.jpg" /></a>
 	<a href="https://mirrors.aliyun.com/apache/hive/" target="_blank"><img border="0" src="./hive.jpg" /></a>
 	<a href="https://mirrors.aliyun.com/apache/spark/" target="_blank"><img border="0" src="./spark.jpg" /></a>
+	<a href="https://mail.qq.com/" target="_blank"><img border="0" src="./qqmail.jpg" /></a>
+	<a href="https://console.cloud.tencent.com/cns/detail/xxy1.ltd/records" target="_blank"><img border="0" src="./tencent_cloud.jpg" />
 	<a href="https://naotu.baidu.com/" target="_blank"><img border="0" src="./baidu_naotu.jpg" /></a>
 	<a href="https://www.right.com.cn/forum/forum-158-1.html" target="_blank"><img border="0" src="./right_ensan.jpg" /></a>
 	<a href="http://192.168.200.201:81" target="_blank"><img border="0" src="./zabbix.jpg" /></a>
 	<a href="https://192.168.200.221/ui/" target="_blank"><img border="0" src="./vmware.jpg" /></a>
 	<a href="https://yadi.sk/d/_rQgn_FosYuW0g" target="_blank"><img border="0" src="./armbian.jpg" /></a>
 	<a href="https://play.google.com/store/apps" target="_blank"><img border="0" src="./google_play.jpg" /></a>
+	<a href="https://translate.google.com/" target="_blank"><img border="0" src="./google_translate.jpg" /></a>
 	<a href="https://www.google.com.hk/" target="_self"><img border="0" src="./google.jpg" /></a>
+	<a href="https://www.freedgo.com/draw-index.html" target="_blank"><img border="0" src="./online_topology_diagram.jpg" /></a>
+	<a href="https://www.waiwang.men/" target="_blank"><img border="0" src="./frp_waiwangmen.jpg" /></a>
+	<a href="http://frp.xxy1.ltd:7777/" target="_blank"><img border="0" src="./frps_dash.jpg" /></a>
 	<br>
 	<a href="https://tmgmatrix.cisco.com/home" target="_blank"><img border="0" src="./cisco_optics_compatibility_matrix.jpg" /></a>
 	<a href="https://www.vmware.com/resources/compatibility/search.php?deviceCategory=io" target="_blank"><img border="0" src="./vmware_compatibility_guide.jpg" /></a>
@@ -77,32 +138,28 @@ if ($Product_Name) { $row_select = "Product_Name" ; }
 elseif ($Item_Number) { $row_select = "Item_Number" ; }
 else { die('<script> alert("无法分辨是物料编号或产品名称 : '. $inputKeywords .'"); </script>'); }
 
-$db_username = "product";
-$db_password = "product";
+$db_username = "10gtek";
+$db_password = "10gtek";
 $db_host = "192.168.200.201";
-$db_name = "product";
-$tab_name = "product_compatibility";
+$db_name = "10gtek_fae";
+$tab_name = "product_data";
 // mysqli_connect(host,username,password,dbname,port,socket);
-$conn_sql = mysqli_connect($db_host, $db_username, $db_password);
-if (!$conn_sql) { 
+$conn_sql = mysqli_connect($db_host, $db_username, $db_password) or  
     die('<script> alert("数据库连接失败 : '. mysqli_connect_error() .'"); </script>');   
-}
 
 mysqli_set_charset($conn_sql, 'utf8');
 mysqli_query($conn_sql, "set names utf8");
 
 //mysqli_select_db(mysqliLink, database)；
 $select = "SELECT * FROM $db_name.$tab_name WHERE $row_select like '%$inputKeywords%'";
-$result = mysqli_query($conn_sql, $select);
-
-if (!$result) { 
+$result = mysqli_query($conn_sql, $select) or
     die('<script> alert("数据库查询失败 : '. mysqli_error($conn_sql) .'"); </script>');
-} //elseif (count(mysqli_fetch_array($result)) < 1) { die('<script> alert("数据库无法找到相关记录"); </script>'); }
+//elseif (count(mysqli_fetch_array($result)) < 1) { die('<script> alert("数据库无法找到相关记录"); </script>'); }
 
 // 下面常量对应数据库的字段，请勿更改数据库字段
-define("itemNum", "Item_Number");
-define("prodName", "Product_Name");
-define("tpwd", "Transceiver Password");
+define("ITEMNUM", "Item_Number");
+define("PRODNAME", "Product_Name");
+define("DESC", "Description");
 
 function tagColor($receive, $swRecord) {
     if (empty($receive)) {
@@ -119,12 +176,12 @@ function tagColor($receive, $swRecord) {
 echo "<fieldset>";
 echo "<table class=outputData border=1 cellspacing=0 cellpadding=3 align=center>";
 echo "<tr class=row1>
-        <th>".itemNum."</th> <th>".prodName."</th> <th>".tpwd."</th>
+        <th>".ITEMNUM."</th> <th>".PRODNAME."</th> <th>".DESC."</th>
         </tr>";
 
 while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) { 
     echo "<tr class=row2>      
-            <td>".$row[itemNum]."</td> <td>".$row[prodName]."</td> <td>".$row[tpwd]."</td>
+            <td>".$row[ITEMNUM]."</td> <td>".$row[PRODNAME]."</td> <td>".$row[DESC]."</td>
             </tr>";
 }
 
@@ -135,6 +192,7 @@ mysqli_free_result($result);
 mysqli_close($conn_sql);
 
 ?>
+
 
 </body> 
 </html>
