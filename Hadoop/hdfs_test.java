c890171a65ef4f1431961542494d272a6b1bdaec
Jun <33471088+151438611@users.noreply.github.com>
2021-05-12 17:24:44 +0800

    Update hdfs_test.java

--- Hadoop/hdfs_test.java
+++ Hadoop/hdfs_test.java
@@ -7,13 +7,14 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-public class HDFS_test {
+public class hdfs_test {
     public static void main(String[] args) {
         FileSystem fileSystem = null;
         try {
             // 在 HDFS 中新建一个 test_dir 文件夹; centos表示hadoop用户名
             fileSystem = FileSystem.get(new URI("hdfs://master:9000"),new Configuration(),"centos");
             fileSystem.mkdirs(new Path("/test_dir"));
+            System.out.println("create dir suesscess! ");
             fileSystem.close();
         } catch (IOException e) {
             e.printStackTrace();
