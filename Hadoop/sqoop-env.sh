ffff1a79adbb7172d1ec3cb128b27456a337e501
Jun <33471088+151438611@users.noreply.github.com>
2021-05-19 16:47:18 +0800

    Update sqoop-env.sh

--- Hadoop/sqoop-env.sh
+++ Hadoop/sqoop-env.sh
@@ -7,12 +7,13 @@
 sqoop import --connect jdbc:mysql://master:3306/db1 --username root --password root --table tb1 --target-dir "/tmp/tb1" --delete-target-dir -m 1 --fields-terminated-by "\t"
 
 # 从 Mysql 中导出 db1.tb1 表到 Hive 中; 需要提前创建Hive数据库,或使用默认数据库default
-sqoop import --connect jdbc:mysql://master:3306/db1 --username root --password root --table tb1 \
---delete-target-dir -m 1 --fields-terminated-by "\t" --hive-import --hive-overwrite --create-hive-table --hive-database default --hive-table htb1 
+sqoop import --connect jdbc:mysql://master:3306/db1 --username root --password root --table tb1 --delete-target-dir -m 1 \
+--fields-terminated-by "\t" --hive-import --hive-overwrite --create-hive-table --hive-database default --hive-table htb1 
 
 # 从Mysql中导出db1.tb1表到HBase中;需要提前创建HBase数据表和列族
 hbase(main):001:0> create 'hbtb1', { NAME => 'f1', VERSIONS => 5}
-sqoop import --connect jdbc:mysql://master:3306/db1 --username root --password root --table tb1 --hbase-table hbtb1 --column-family f1 --hbase-row-key id --hbase-create-table -m 1
+sqoop import --connect jdbc:mysql://master:3306/db1 --username root --password root --table tb1 \
+--hbase-table hbtb1 --column-family f1 --hbase-row-key id --hbase-create-table -m 1
  
 
 #Set path to where bin/hadoop is available
