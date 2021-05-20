abfaa331e263ff0465855b523099b8eede3e73ca
Jun <33471088+151438611@users.noreply.github.com>
2021-05-20 09:23:13 +0800

    Update WordCountReducer.java

--- Hadoop/wordCount/WordCountReducer.java
+++ Hadoop/wordCount/WordCountReducer.java
@@ -1,13 +1,11 @@
 package wordCount;
 
 import java.io.IOException;
-
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class WordCountReducer extends Reducer<Text, IntWritable, Text,IntWritable> {
-
 	public void reduce(Text _key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
 		// process values
 		int count=0;
@@ -17,5 +15,4 @@ public class WordCountReducer extends Reducer<Text, IntWritable, Text,IntWritabl
 		}
 		context.write(_key,new IntWritable(count));
 	}
-
 }
