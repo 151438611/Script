a0f32c7d8578fb6a553625a32e4005ccecef6cb1
Jun <33471088+151438611@users.noreply.github.com>
2021-05-20 16:16:32 +0800

    Update mysqldump.sh

--- nginx_php_mysql/mysqldump.sh
+++ nginx_php_mysql/mysqldump.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 因crontab中直接运行mysqldump不生效，使用脚本操作正常
+# 因crontab中直接运行mysqldump命令不生效，使用脚本操作正常
 
 mysql_user=root
 mysql_password=root
@@ -7,7 +7,5 @@ databases="product os"
 
 for db in $databases
 do
-
-  /usr/bin/mysqldump -u$mysql_user -p$mysql_password --events --master-data=2 -x -F -B $db > /opt/${db}_mysqldump_$(date +%Y%m%d).sql
-
+  /usr/bin/mysqldump -u$mysql_user -p$mysql_password --events --master-data=2 -x -F -B $db > /opt/mysqldump_$db_$(date +%Y%m%d).sql
 done
