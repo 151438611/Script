645a13b5ae8b80f885317880f70bacdf6de785ff
Jun <33471088+151438611@users.noreply.github.com>
2021-05-20 09:22:01 +0800

    Update FlowReducer.java

--- Hadoop/flow/FlowReducer.java
+++ Hadoop/flow/FlowReducer.java
@@ -1,13 +1,11 @@
 package flow;
 
 import java.io.IOException;
-
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class FlowReducer extends Reducer<Text, Flow, Text, IntWritable> {
-
 	public void reduce(Text _key, Iterable<Flow> values, Context context) throws IOException, InterruptedException {
 		// process values
 		int sum = 0;
@@ -18,5 +16,4 @@ public class FlowReducer extends Reducer<Text, Flow, Text, IntWritable> {
 		}
 		context.write(new Text(_key.toString()+"-"+name), new IntWritable(sum));
 	}
-
 }
