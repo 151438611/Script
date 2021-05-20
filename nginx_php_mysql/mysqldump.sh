ceb061838b1ce9361d2dc94d8600f841a0f60088
Jun <33471088+151438611@users.noreply.github.com>
2021-05-20 16:17:15 +0800

    Update mysqldump.sh

--- nginx_php_mysql/mysqldump.sh
+++ nginx_php_mysql/mysqldump.sh
@@ -3,9 +3,10 @@
 
 mysql_user=root
 mysql_password=root
+mysql_host=localhost
 databases="product os"
 
 for db in $databases
 do
-  /usr/bin/mysqldump -u$mysql_user -p$mysql_password --events --master-data=2 -x -F -B $db > /opt/mysqldump_$db_$(date +%Y%m%d).sql
+  /usr/bin/mysqldump -u$mysql_user -p$mysql_password -h $mysql_host --events --master-data=2 -x -F -B $db > /opt/mysqldump_$db_$(date +%Y%m%d).sql
 done
