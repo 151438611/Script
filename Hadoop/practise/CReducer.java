70b979265ced5c342f74c09af6bcc8aef96a3ccf
Jun <33471088+151438611@users.noreply.github.com>
2021-05-20 09:22:39 +0800

    Update CReducer.java

--- Hadoop/practise/CReducer.java
+++ Hadoop/practise/CReducer.java
@@ -1,21 +1,17 @@
 package practise;
 
 import java.io.IOException;
-
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class CReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
-
 	public void reduce(Text _key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
 		// process values
 		int count = 0;
 		for (IntWritable val : values) {
 			count+=val.get();
-
 		}
 		context.write(_key, new IntWritable(count));
 	}
-
 }
