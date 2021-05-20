f6b4dd4bf19b8c957d2f970ee8f8c2c4558a7b77
Jun <33471088+151438611@users.noreply.github.com>
2021-05-20 16:23:53 +0800

    Update mysqldump.sh

--- nginx_php_mysql/mysqldump.sh
+++ nginx_php_mysql/mysqldump.sh
@@ -4,13 +4,13 @@
 mysql_user=root
 mysql_password=root
 mysql_host=localhost
-databases="product os"
+dump_databases="product os"
 
 # 将多个数据库存备份到单个指定文件中
-# /usr/bin/mysqldump -u$mysql_user -p$mysql_password -h $mysql_host --events --master-data=2 -x -F -B $databases > /opt/mysqldump_databases_$(date +%Y%m%d).sql
+# /usr/bin/mysqldump -u$mysql_user -p$mysql_password -h $mysql_host --events --master-data=2 -x -F -B $dump_databases > /opt/mysqldump_databases_$(date +%Y%m%d).sql
 
 # 将多个数据库存备份到不同的文件中
-for db in $databases
+for db in $dump_databases
 do
   /usr/bin/mysqldump -u$mysql_user -p$mysql_password -h $mysql_host --events --master-data=2 -x -F -B $db > /opt/mysqldump_${db}_$(date +%Y%m%d).sql
 done
