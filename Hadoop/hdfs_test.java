503588985617c00308f71ee1ad27e66f8b902b0f
Jun <33471088+151438611@users.noreply.github.com>
2021-05-21 12:00:32 +0800

    Update hdfs_test.java

--- Hadoop/hdfs_test.java
+++ Hadoop/hdfs_test.java
@@ -1,4 +1,3 @@
-// 测试OK
 package hadoop;
 
 import org.apache.hadoop.conf.Configuration;
@@ -7,29 +6,28 @@ import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.Path;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 
-public class hdfs_test {
-    
+public class test_hdfs {
 	public static void main(String[] args) {
-        FileSystem fileSystem = null;
-        try {
-            // 在 HDFS 中新建一个 test_dir 文件夹; centos表示hadoop用户名
-            fileSystem = FileSystem.get(new URI("hdfs://node1:9000"),new Configuration(),"ubuntu");
-            fileSystem.mkdirs(new Path("/test_dir1"));
-            System.out.println("create dir /test_dir1 suesscess!");
-            fileSystem.mkdirs(new Path("/test_dir2"));
-            System.out.println("create dir /test_dir2 suesscess!");
-            fileSystem.delete(new Path("/test_dir2"));
-            System.out.println("delete dir /test_dir2 suesscess!");
-            fileSystem.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
+	try {
+		Configuration conf=new Configuration();
+		conf.set("fs.defaultFS", "hdfs://node1:9000");
+		System.setProperty("HADOOP_USER_NAME", "ubuntu");
+        FileSystem fs=FileSystem.get(conf);
+        
+        fs.mkdirs(new Path("/test_dir1"));
+	    System.out.println("create dir /test_dir1 suesscess!");
+	    
+	    fs.mkdirs(new Path("/test_dir2"));
+	    System.out.println("create dir /test_dir2 suesscess!");
+	    
+	    fs.delete(new Path("/test_dir2"));
+	    System.out.println("delete dir /test_dir2 suesscess!");
+	    //fs.listStatus(new Path("/test_dir2"));	
+	    
+	    fs.close();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
     }
 }
