835b329b40f260e68075a0f0b23aeccd4ad928b9
Jun <33471088+151438611@users.noreply.github.com>
2021-05-28 10:16:09 +0800

    Update FlowDriver.java

--- Hadoop/flow/FlowDriver.java
+++ Hadoop/flow/FlowDriver.java
@@ -1,5 +1,5 @@
 // 手机流量排序
-package practise;
+package flow;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -10,39 +10,35 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.log4j.BasicConfigurator;
 
-public class CDriver {
+public class FlowDriver {
+
 	public static void main(String[] args) throws Exception {
-		BasicConfigurator.configure();
-		
 		Configuration conf = new Configuration();
-		conf.set("fs.defaultFS", "hdfs://master:9000");
-        	//conf.set("yarn.resourcemanager.hostname","master");
+		conf.set("fs.defaultFS", "hdfs://hadoop:9000");
+        //conf.set("yarn.resourcemanager.hostname","master");
         
-		Job job = Job.getInstance(conf, "CharacterCount");
-		job.setJar("D:\\eclipse\\export_tmp\\CDriver.jar");
-		job.setJarByClass(practise.CDriver.class);
+		Job job = Job.getInstance(conf, "flow");
+		job.setJar("D:\\eclipse\\export_tmp\\FlowDriver.jar");
+		
+		job.setJarByClass(flow.FlowDriver.class);
 		// TODO: specify a mapper
-		job.setMapperClass(CMapper.class);
+		job.setMapperClass(FlowMapper.class);
 		// TODO: specify a reducer
-		job.setReducerClass(CReducer.class);
+		job.setReducerClass(FlowReducer.class);
 		
 		job.setMapOutputKeyClass(Text.class);
-		job.setMapOutputValueClass(IntWritable.class);
+		job.setMapOutputValueClass(Flow.class);
 
 		// TODO: specify output types
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
 
 		// TODO: specify input and output DIRECTORIES (not files)
-		FileInputFormat.setInputPaths(job, new Path("hdfs://master:9000/input/characters.txt"));
-		FileOutputFormat.setOutputPath(job, new Path("hdfs://master:9000/characterCount1"));
-		//FileInputFormat.addInputPath(job, new Path(args[0]));
-        	//FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		FileInputFormat.setInputPaths(job, new Path("hdfs://hadoop:9000/flow.txt"));
+		FileOutputFormat.setOutputPath(job, new Path("hdfs://hadoop:9000/FlowResult1"));
 
 		if (!job.waitForCompletion(true))
 			return;
 	}
-
 }
